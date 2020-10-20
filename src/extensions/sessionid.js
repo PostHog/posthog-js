30cde3012bb7fab6e37ebb15761044f9d3d44c55
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-10-20 13:18:32 +0300

    Rename a const

--- src/extensions/sessionid.js
+++ src/extensions/sessionid.js
@@ -1,12 +1,12 @@
 import { SESSION_ID } from '../posthog-persistence'
 import { _ } from '../utils'
 
-const SESSION_CHANGE_TIMEOUT = 30 * 60 * 1000 // 30 mins
+const SESSION_CHANGE_THRESHOLD = 30 * 60 * 1000 // 30 mins
 
 export default (persistence, timestamp) => {
     let [lastTimestamp, sessionId] = persistence['props'][SESSION_ID] || [0, null]
 
-    if (Math.abs(timestamp - lastTimestamp) > SESSION_CHANGE_TIMEOUT) {
+    if (Math.abs(timestamp - lastTimestamp) > SESSION_CHANGE_THRESHOLD) {
         sessionId = _.UUID()
     }
 
