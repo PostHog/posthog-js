d9049ca984a118a869f289287e40e98a641f8f00
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-26 23:24:03 +0200

    Bundle module.js to es2015

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default {
     plugins: [
         json(),
-        resolve({
-            browser: true,
-            main: true,
-            jsnext: true,
-        }),
+        resolve({ browser: true, modulesOnly: true }),
+        babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
     ],
 }
