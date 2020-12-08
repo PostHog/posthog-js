00692b63a65ae7fb2450413379354ca7eff9b2ff
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-12-08 13:46:22 +0200

    WIP: use rollup to package everything
    Before:
    
    ```
    -rw-rw-r--  1 macobo macobo  77K dets   8 13:45 array.js
    -rw-rw-r--  1 macobo macobo 167K dets   8 13:45 module.js
    ```
    
    After:
    ```
    -rw-rw-r--  1 macobo macobo  62K dets   8 13:46 array.js
    -rw-rw-r--  1 macobo macobo  62K dets   8 13:46 module.js
    ```

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,31 @@
 import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 
-export default {
-    plugins: [
-        json(),
-        resolve({ browser: true, modulesOnly: true }),
-        babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
-    ],
-}
+const plugins = [
+    json(),
+    resolve({ browser: true, modulesOnly: true }),
+    babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
+    terser({ ecma: 5 }),
+]
+
+const configs = []
+configs.push({
+    input: 'src/loader-globals.js',
+    output: {
+        file: 'dist/array.js',
+        format: 'esm',
+    },
+    plugins,
+})
+configs.push({
+    input: 'src/loader-module.js',
+    output: {
+        file: 'dist/module.js',
+        format: 'esm',
+    },
+    plugins,
+})
+
+export default configs
