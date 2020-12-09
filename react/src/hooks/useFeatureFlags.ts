6e16d27ad9b8a8faaf2ee04e6e2c2a011cd0514a
Steven Phaedonos <steven@phaedonos.com>
2020-12-09 20:19:16 +0300

    Update useFeatureFlags to use posthog methods

--- react/src/hooks/useFeatureFlags.ts
+++ react/src/hooks/useFeatureFlags.ts
@@ -5,39 +5,37 @@ import { usePostHogContext, FeatureFlags } from '../context'
  * A hook that fetches active feature flags and determines which flags are enabled for the user.
  * @param props.refreshInterval - How often to refresh the feature flags, in seconds.
  * @param props.sendEvent - A flag that controls whether an event will be sent on flag refresh.
- * @returns An object containing flags that are enabled for the user.
+ * @returns An object containing active flags and flags that are enabled for the user.
  */
-export function useFeatureFlags(props: { refreshInterval: number; sendEvent: boolean }): FeatureFlags['enabled'] {
+export function useFeatureFlags(props: { refreshInterval: number; sendEvent: boolean }): FeatureFlags {
     const { refreshInterval = 0, sendEvent = true } = props || {}
     const { client: posthog, featureFlags, setFeatureFlags } = usePostHogContext()
 
-    const getFeatureFlags = useCallback((): void => {
-        if (posthog) {
-            const active = featureFlags.active || posthog.featureFlags.getFlags()
-
-            const enabled = active.reduce((result: FeatureFlags['enabled'], flag: string) => {
-                result[flag] = !!posthog.isFeatureEnabled(flag, {
+    const getEnabledFlags = useCallback(
+        (flags): void => {
+            const enabled = flags.reduce((result: FeatureFlags['enabled'], flag: string) => {
+                result[flag] = !!posthog?.isFeatureEnabled(flag, {
                     send_event: sendEvent,
                 })
                 return result
             }, {})
-
-            setFeatureFlags({ active, enabled })
-        }
-    }, [posthog, featureFlags.active, sendEvent, setFeatureFlags])
-
-    useEffect(() => {
-        if (posthog && !featureFlags.active) getFeatureFlags()
-    }, [posthog, featureFlags.active, getFeatureFlags])
+            setFeatureFlags({ active: flags, enabled })
+        },
+        [posthog, sendEvent, setFeatureFlags]
+    )
 
     useEffect(() => {
-        if (refreshInterval > 0) {
+        if (posthog && refreshInterval > 0) {
             const interval = setInterval(() => {
-                getFeatureFlags()
+                posthog?.featureFlags.reloadFeatureFlags()
             }, refreshInterval * 1000)
             return () => clearInterval(interval)
         }
-    }, [refreshInterval, getFeatureFlags])
+    }, [posthog, refreshInterval, getEnabledFlags])
 
-    return featureFlags.enabled
+    useEffect(() => {
+        posthog?.onFeatureFlags(getEnabledFlags)
+    }, [posthog, getEnabledFlags])
+
+    return featureFlags
 }
