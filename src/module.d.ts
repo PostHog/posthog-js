827f51972af36dd397a0c6e9e313524d5fc60891
Michael Matloka <dev@twixes.com>
2021-03-12 18:37:49 +0100

    Fix SentryIntegration optional param typing

--- src/module.d.ts
+++ src/module.d.ts
@@ -452,23 +452,23 @@ declare class posthog {
     static onFeatureFlags(callback: (flags: string[]) => void): false | undefined
 
     /**
-         * Integrate Sentry with PostHog. This will add a direct link to the person in Sentry, and an $exception event in PostHog
-         *
-         * ### Usage
-         *
-         *     Sentry.init({
-         *          dsn: 'https://example',
-         *          integrations: [
-         *              new posthog.SentryIntegration(posthog)
-         *          ]
-         *     })
-         *
-         * @param {Object} [posthog] The posthog object
-         * @param {string} [organization] Optional: The Sentry organization, used to send a direct link from PostHog to Sentry
-         * @param {Number} [projectId] Optional: The Sentry project id, used to send a direct link from PostHog to Sentry
-         * @param {string} [prefix] Optional: Url of a self-hosted sentry instance (default: https://sentry.io/organizations/)
+     * Integrate Sentry with PostHog. This will add a direct link to the person in Sentry, and an $exception event in PostHog
+     *
+     * ### Usage
+     *
+     *     Sentry.init({
+     *          dsn: 'https://example',
+     *          integrations: [
+     *              new posthog.SentryIntegration(posthog)
+     *          ]
+     *     })
+     *
+     * @param {Object} [posthog] The posthog object
+     * @param {string} [organization] Optional: The Sentry organization, used to send a direct link from PostHog to Sentry
+     * @param {Number} [projectId] Optional: The Sentry project id, used to send a direct link from PostHog to Sentry
+     * @param {string} [prefix] Optional: Url of a self-hosted sentry instance (default: https://sentry.io/organizations/)
      */
-    static SentryIntegration(posthog: posthog, organization: string, projectId: number, prefix: string): void
+    static SentryIntegration(posthog: posthog, organization?: string, projectId?: number, prefix?: string): void
 
     static toString(): string
 
