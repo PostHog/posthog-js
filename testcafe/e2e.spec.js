ff94c25316abffe96051ca77b7ba2d808ec98f87
yakkomajuri <yakko.majuri@gmail.com>
2021-04-06 14:55:56 +0000

    update tests

--- testcafe/e2e.spec.js
+++ testcafe/e2e.spec.js
@@ -17,7 +17,7 @@ fixture('posthog.js capture')
         console.debug('Requests to posthog:', JSON.stringify(captureLogger.requests, null, 2))
     })
 
-test('Captured events are accessible via /api/event', async (t) => {
+test('Custom events work and are accessible via /api/event', async (t) => {
     await initPosthog()
     await t
         .click('[data-cy-custom-event-button]')
@@ -35,45 +35,53 @@ test('Captured events are accessible via /api/event', async (t) => {
     await t.expect(results.filter(({ event }) => event === '$pageview').length).gte(1)
 })
 
-test('Config option mask_all_text does not set $el_text for autocaptured events', async (t) => {
-    await initPosthog({ mask_all_text: true })
+test('Autocaptured events work and are accessible via /api/event', async (t) => {
+    await initPosthog()
     await t
         .click('[data-cy-link-mask-text]')
+        .click('[data-cy-button-sensitive-attributes]')
         .wait(5000)
         .expect(captureLogger.count(() => true))
-        .gte(1)
+        .gte(2)
 
     // Check no requests failed
     await t.expect(captureLogger.count(({ response }) => response.statusCode !== 200)).eql(0)
 
     const results = await retryUntilResults(queryAPI)
 
-    const autocapturedElement = results.filter((e) => e.event === '$autocapture')[0].elements[0]
+    const autocapturedEvents = results.filter((e) => e.event === '$autocapture')
+    await t.expect(autocapturedEvents.length).eql(2)
 
-    // $pageview + $autocapture
-    await t.expect(results.length).gte(2)
+    const autocapturedButtonClick = autocapturedEvents.filter((e) => e.elements[0].tag_name === 'a')
+    const autocapturedLinkClick = autocapturedEvents.filter((e) => e.elements[0].tag_name === 'button')
 
-    await t.expect(autocapturedElement.tag_name).eql('a')
-    await t.expect('$el_text' in autocapturedElement).eql(false)
+    await t.expect(autocapturedButtonClick.length).eql(1)
+    await t.expect(autocapturedLinkClick.length).eql(1)
 })
 
-test('Config option mask_all_element_attributes does not capture any attributes for autocaptured elements', async (t) => {
-    await initPosthog({ mask_all_element_attributes: true })
+test('Config options change autocapture behavior accordingly', async (t) => {
+    await initPosthog({ mask_all_text: true, mask_all_element_attributes: true })
     await t
+        .click('[data-cy-link-mask-text]')
         .click('[data-cy-button-sensitive-attributes]')
-        .wait(8000)
+        .wait(5000)
         .expect(captureLogger.count(() => true))
-        .gte(1)
+        .gte(2)
 
     // Check no requests failed
     await t.expect(captureLogger.count(({ response }) => response.statusCode !== 200)).eql(0)
 
     const results = await retryUntilResults(queryAPI)
 
-    const autocapturedElement = results.filter((e) => e.event === '$autocapture')[0].elements[0]
+    const autocapturedEvents = results.filter((e) => e.event === '$autocapture')
+    await t.expect(autocapturedEvents.length).eql(2)
 
-    // $pageview + $autocapture
-    await t.expect(results.length).gte(2)
+    const autocapturedButtonElement = autocapturedEvents.filter((e) => e.elements[0].tag_name === 'a')[0].elements[0]
+    const autocapturedLinkElement = autocapturedEvents.filter((e) => e.elements[0].tag_name === 'button')[0].elements[0]
 
-    await t.expect(Object.keys(autocapturedElement.attributes).length).eql(0)
+    // mask_all_text does not set $el_text
+    await t.expect('$el_text' in autocapturedLinkElement).eql(false)
+
+    // mask_all_element_attributes does not capture any attributes at all from all elements
+    await t.expect(Object.keys(autocapturedButtonElement.attributes).length).eql(0)
 })
