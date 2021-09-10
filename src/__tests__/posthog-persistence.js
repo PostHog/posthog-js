2724334fb114450c82af056eb394c22eac653ef7
Marius Andra <marius.andra@gmail.com>
2021-09-10 19:54:03 +0200

    Split feature flags into `$feature/*` properties (#278)
    * split enabled feature flags into separate properties
    
    * reset persistence on each run
    
    * drop cache after test

--- src/__tests__/posthog-persistence.js
+++ src/__tests__/posthog-persistence.js
@@ -3,6 +3,10 @@ import { PostHogPersistence } from '../posthog-persistence'
 given('lib', () => new PostHogPersistence({ name: 'bla', persistence: 'cookie' }))
 
 describe('persistence', () => {
+    afterEach(() => {
+        given.lib.clear()
+    })
+
     it('should set referrer', () => {
         // Initial visit
         given.lib.update_referrer_info('https://www.google.com')
@@ -26,5 +30,14 @@ describe('persistence', () => {
         // last touch should still be set to facebook
         expect(given.lib.props['$referring_domain']).toBe('www.facebook.com')
         expect(given.lib.props['$referrer']).toBe('https://www.facebook.com')
+    })
+
+    it('extracts enabled feature flags', () => {
+        given.lib.register({ $enabled_feature_flags: { flag: 'variant', other: true } })
+        expect(given.lib.props['$enabled_feature_flags']).toEqual({ flag: 'variant', other: true })
+        expect(given.lib.properties()).toEqual({
+            '$feature/flag': 'variant',
+            '$feature/other': true,
+        })
     })
 })
