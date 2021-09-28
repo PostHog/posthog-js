c7a7c77a92e03f93afcfa85c9a44f9dcf441bd96
Paul D'Ambra <paul.dambra@gmail.com>
2021-09-28 09:57:24 +0100

    speculative logging for https://github.com/PostHog/posthog/issues/4816

--- src/compression.js
+++ src/compression.js
@@ -12,15 +12,40 @@ export function decideCompression(compressionSupport) {
     }
 }
 
+function hasMagicGzipHeader(compressionResultElement) {
+    try {
+        const a = compressionResultElement[0]
+        const b = compressionResultElement[1]
+        return a === 31 && b === 139
+    } catch (e) {
+        return false
+    }
+}
+
 export function compressData(compression, jsonData, options) {
     if (compression === 'lz64') {
         return [{ data: LZString.compressToBase64(jsonData), compression: 'lz64' }, options]
     } else if (compression === 'gzip-js') {
         // :TRICKY: This returns an UInt8Array. We don't encode this to a string - returning a blob will do this for us.
-        return [
+        const compressionResult = [
             gzipSync(strToU8(jsonData), { mtime: 0 }),
             { ...options, blob: true, urlQueryArgs: { compression: 'gzip-js' } },
         ]
+
+        // temporary logging to identify source of https://github.com/PostHog/posthog/issues/4816
+        if (
+            !jsonData ||
+            jsonData === 'undefined' ||
+            !compressionResult[0] ||
+            !hasMagicGzipHeader(compressionResult[0])
+        ) {
+            posthog.capture('PostHogJSCompressionCannotBeDecompressed', {
+                jsonData,
+                compressionResult: compressionResult[0],
+            })
+        }
+
+        return compressionResult
     } else {
         return [{ data: _.base64Encode(jsonData) }, options]
     }
