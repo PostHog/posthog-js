85e5d38690e7e5b526f81b6f89d34fac8f6a082b
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-10-26 11:59:42 +0300

    Update varsignorepattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
     plugins: ['prettier'],
     rules: {
         'prettier/prettier': 'error',
-        'no-unused-vars': ['error', { ignoreRestSiblings: true, varsIgnorePattern: /^_.*/ }],
+        'no-unused-vars': ['error', { ignoreRestSiblings: true, varsIgnorePattern: '^_.*' }],
     },
 }
