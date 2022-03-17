88215664a4cd54527b5fd16b71df40ecfa91a516
Rick Marron <rcmarron@gmail.com>
2022-03-17 14:31:36 -0600

    feat: add recording url to sentry integration

--- src/posthog-core.js
+++ src/posthog-core.js
@@ -1494,7 +1494,9 @@ PostHogLib.prototype.sentry_integration = function (_posthog, organization, proj
         addGlobalEventProcessor((event) => {
             if (event.level !== 'error' || !_posthog.__loaded) return event
             if (!event.tags) event.tags = {}
-            event.tags['PostHog URL'] = _posthog.config.api_host + '/person/' + _posthog.get_distinct_id()
+            event.tags['PostHog Person URL'] = _posthog.config.api_host + '/person/' + _posthog.get_distinct_id()
+            event.tags['PostHog Recording URL'] =
+                _posthog.config.api_host + '/recordings/#sessionRecordingId=' + _posthog.sessionManager.sessionId
             let data = {
                 $sentry_event_id: event.event_id,
                 $sentry_exception: event.exception,
