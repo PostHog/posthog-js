8af2569265a8241d949834dbb2c46993fa406a17
Paul D'Ambra <paul.dambra@gmail.com>
2022-03-22 10:50:39 +0000

    feat: send library version outside of compressed body as a debug signal (#351)
    * send lib version in http header
    
    * no need to use given to change status code
    
    * fix cypress tests
    
    * switch to sending version in user agent
    
    * send web not posthog-js
    
    * fix _all_ of the cypress tests
    
    * replace possibly dangerous user-agent header with posthog-user-agent
    
    * switch to adding library and version in the header of posthog API calls
    
    * put config in front of sending library and version in query params
    
    * Revert "put config in front of sending library and version in query params"
    
    This reverts commit 5e14120cb98f39e12d1a5cf293062b5f0f81d474.
    
    * don't send web as library in URL

--- src/send-request.js
+++ src/send-request.js
@@ -1,4 +1,15 @@
 import { _, logger } from './utils'
+import Config from './config'
+
+export const addParamsToURL = (url, urlQueryArgs, parameterOptions) => {
+    const args = urlQueryArgs || {}
+    args['ip'] = parameterOptions['ip'] ? 1 : 0
+    args['_'] = new Date().getTime().toString()
+    args['v'] = Config.LIB_VERSION
+
+    const argSeparator = url.indexOf('?') > -1 ? '&' : '?'
+    return url + argSeparator + _.HTTPBuildQuery(args)
+}
 
 export const encodePostData = (data, options) => {
     if (options.blob && data.buffer) {
@@ -51,6 +62,7 @@ export const xhr = ({
     _.each(headers, function (headerValue, headerName) {
         req.setRequestHeader(headerName, headerValue)
     })
+
     if (options.method === 'POST' && !options.blob) {
         req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
     }
