75cd657b21e859ccde3165d6ea06b47a02ff13ef
timgl <tim@glsr.nl>
2022-08-15 15:50:52 +0000

    fix: Use rollup and fix define module issues

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,31 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import pkg from './package.json'
+// import { terser } from 'rollup-plugin-terser'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
 export default [
+    {
+        input: 'src/loader-globals.ts',
+        output: [
+            {
+                file: 'array.js',
+                sourcemap: true,
+                format: 'umd',
+                name: 'posthog' ,
+                amd: {
+                    id: 'my-bundle'
+                  }
+            }
+        ],
+        plugins: [
+            json(),
+            resolve({ browser: true, modulesOnly: true }),
+            typescript({ sourceMap: true }),
+            babel({ extensions, babelHelpers: 'bundled', presets: ['@babel/preset-env'] })
+        ]
+    },
     {
         input: 'src/loader-module.ts',
         output: [
@@ -34,5 +55,5 @@ export default [
         input: './lib/src/loader-module.d.ts',
         output: [{ file: pkg.types, format: 'es' }],
         plugins: [dts()],
-    },
+    }, 
 ]
