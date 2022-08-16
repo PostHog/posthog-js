a2a989844531a444a9d89bf3b985ec278b943926
Marius Andra <marius.andra@gmail.com>
2022-08-16 10:37:34 +0200

    move to iife export

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,32 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import pkg from './package.json'
-// import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const plugins = [
+    json(),
+    resolve({ browser: true, modulesOnly: true }),
+    typescript({ sourceMap: true }),
+    babel({
+        extensions,
+        babelHelpers: 'bundled',
+        presets: ['@babel/preset-env'],
+    }),
+]
 
 export default [
     {
         input: 'src/loader-globals.ts',
         output: [
             {
-                file: 'array.js',
+                file: 'dist/array.js',
                 sourcemap: true,
-                format: 'umd',
-                name: 'posthog' ,
-                amd: {
-                    id: 'my-bundle'
-                  }
-            }
+                format: 'iife',
+                name: 'posthog',
+            },
         ],
-        plugins: [
-            json(),
-            resolve({ browser: true, modulesOnly: true }),
-            typescript({ sourceMap: true }),
-            babel({ extensions, babelHelpers: 'bundled', presets: ['@babel/preset-env'] })
-        ]
+        plugins: [...plugins, terser({ toplevel: true })],
     },
     {
         input: 'src/loader-module.ts',
@@ -44,16 +46,11 @@ export default [
                 sourcemap: true,
             },
         ],
-        plugins: [
-            json(),
-            resolve({ browser: true, modulesOnly: true }),
-            typescript({ sourceMap: true }),
-            babel({ extensions, babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
-        ],
+        plugins,
     },
     {
         input: './lib/src/loader-module.d.ts',
         output: [{ file: pkg.types, format: 'es' }],
         plugins: [dts()],
-    }, 
+    },
 ]
