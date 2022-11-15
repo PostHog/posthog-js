040d85558544abb658feb4f7a6151ba7b855050b
Ben White <ben@posthog.com>
2022-11-15 14:34:07 +0100

    Fixed comments

--- src/extensions/segment-integration.ts
+++ src/extensions/segment-integration.ts
@@ -3,17 +3,18 @@
  *
  * ### Usage
  *
- *     Sentry.init({
- *          dsn: 'https://example',
- *          integrations: [
- *              new posthog.SentryIntegration(posthog)
- *          ]
- *     })
+ *  ```js
+ *  // After your standard segment anyalytics install
+ *  analytics.load("GOEDfA21zZTtR7clsBuDvmBKAtAdZ6Np");
  *
- * @param {Object} [posthog] The posthog object
- * @param {string} [organization] Optional: The Sentry organization, used to send a direct link from PostHog to Sentry
- * @param {Number} [projectId] Optional: The Sentry project id, used to send a direct link from PostHog to Sentry
- * @param {string} [prefix] Optional: Url of a self-hosted sentry instance (default: https://sentry.io/organizations/)
+ *  analytics.ready(() => {
+ *    posthog.init('<posthog-api-key>', {
+ *      capture_pageview: false,
+ *      segment: window.analytics, // NOTE: Be sure to use window.analytics here!
+ *    });
+ *    window.analytics.page();
+ *  })
+ *  ```
  */
 import { PostHog } from '../posthog-core'
 
