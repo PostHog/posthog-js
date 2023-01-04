ea08d428788d2a2d8b2e46598f7fe02fe17fd6ab
Neil Kakkar <neilkakkar@gmail.com>
2023-01-04 15:03:38 +0000

    fix(toolbar): Load toolbar only in body for turbolink

--- src/autocapture-utils.ts
+++ src/autocapture-utils.ts
@@ -303,14 +303,14 @@ export function isAngularStyleAttr(attributeName: string): boolean {
     return false
 }
 
-export function loadScript(scriptUrlToLoad: string, callback: (event: Event) => void): void {
+export function loadScript(scriptUrlToLoad: string, callback: (event: Event) => void, restrictToHTMLBody?: boolean): void {
     const scriptTag = document.createElement('script')
     scriptTag.type = 'text/javascript'
     scriptTag.src = scriptUrlToLoad
     scriptTag.onload = callback
 
     const scripts = document.getElementsByTagName('script')
-    if (scripts.length > 0) {
+    if (scripts.length > 0 && !restrictToHTMLBody) {
         scripts[0].parentNode?.insertBefore(scriptTag, scripts[0])
     } else {
         document.body.appendChild(scriptTag)
