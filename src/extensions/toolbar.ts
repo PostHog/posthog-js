afa45b62c7c21da57fb1590cbc288d4bea2e1768
Paul D'Ambra <paul.dambra@gmail.com>
2023-01-12 10:32:30 +0000

    fix: use django cache for toolbar js (#503)
    In #464 (probably) we started serving toolbar.js from app-static-prod cdn. That CDN ignores queryparams and caches by filename for 24 hours.
    
    When we released PostHog/posthog#13620 this meant that the toolbar was effectively broken as the API had changed but the served JS had not.
    
    This has two changes:
    
    first, don't use the jsURL for the toolbar.js
    second, only change the query param once every 30 seconds to reduce load on django

--- src/extensions/toolbar.ts
+++ src/extensions/toolbar.ts
@@ -103,8 +103,14 @@ export class Toolbar {
         // only load the toolbar once, even if there are multiple instances of PostHogLib
         ;(window as any)['_postHogToolbarLoaded'] = true
 
-        const host = params?.['jsURL'] || params?.['apiURL'] || this.instance.get_config('api_host')
-        const toolbarUrl = `${host}${host.endsWith('/') ? '' : '/'}static/toolbar.js?_ts=${new Date().getTime()}`
+        // the toolbar does not use the `jsURL` as that route is cached for 24 hours.
+        // By design array.js, recorder.js, and toolbar.js are served from Django with no or limited caching, not from our CDN
+        // Django respects the query params for caching, returning a 304 if appropriate
+        const host = params?.['apiURL'] || this.instance.get_config('api_host')
+        const timestampToNearestThirtySeconds = Math.floor(Date.now() / 30000) * 30000
+        const toolbarUrl = `${host}${
+            host.endsWith('/') ? '' : '/'
+        }static/toolbar.js?_ts=${timestampToNearestThirtySeconds}`
         const disableToolbarMetrics =
             !POSTHOG_MANAGED_HOSTS.includes(this.instance.get_config('api_host')) &&
             this.instance.get_config('advanced_disable_toolbar_metrics')
