ce9c5d66ba91e18f9d1b186716d9ed15b166f863
Paul D'Ambra <paul@posthog.com>
2023-02-02 10:56:06 +0000

    Add an end to end test

--- cypress/e2e/identify.cy.js
+++ cypress/e2e/identify.cy.js
@@ -12,4 +12,59 @@ describe('identify()', () => {
         cy.posthog().invoke('identify', 'some-id')
         cy.posthog().invoke('opt_out_capturing')
     })
+
+    it('merges people as expected', () => {
+        cy.posthog().invoke('capture', 'an-anonymous-event')
+        cy.posthog().invoke('identify', 'first-identify')
+        cy.posthog().invoke('capture', 'an-identified-event')
+        cy.posthog().invoke('identify', 'second-identify-should-be-ignored')
+        cy.posthog().invoke('reset')
+        cy.posthog().invoke('capture', 'an-anonymous-event')
+        cy.posthog().invoke('identify', 'third-identify')
+        cy.posthog().invoke('capture', 'an-identified-event')
+
+        cy.phCaptures({ full: true }).then((events) => {
+            const eventsSeen = events.map((e) => e.event)
+
+            expect(eventsSeen.filter((e) => e === '$identify').length).to.eq(2)
+
+            expect(eventsSeen).to.deep.eq([
+                '$pageview',
+                'an-anonymous-event',
+                '$identify',
+                'an-identified-event',
+                'an-anonymous-event',
+                '$identify',
+                'an-identified-event',
+            ])
+
+            console.log(
+                events.map((e) => ({
+                    deviceId: e.properties['$device_id'],
+                    distinctId: e.properties.distinct_id,
+                    anonDistinctId: e.properties['$anon_distinct_id'],
+                    name: e.event,
+                }))
+            )
+
+            expect(new Set(events.map((e) => e.properties['$device_id'])).size).to.eql(1)
+
+            // the first two events share a distinct id
+            expect(events[0].properties.distinct_id).to.eql(events[1].properties.distinct_id)
+            // then first identify is called and sends that distinct id as its anon to merge
+            expect(events[2].properties.distinct_id).to.eql('first-identify')
+            expect(events[2].properties['$anon_distinct_id']).to.eql(events[0].properties.distinct_id)
+            // and an event is sent with that distinct id
+            expect(events[3].properties.distinct_id).to.eql('first-identify')
+            // then second identify is called and is ignored
+            // then reset is called and the next event has a new distinct id
+            expect(events[4].event).not.to.eql('$identify')
+            expect(events[4].properties.distinct_id).not.to.eql('first-identify')
+            // then an identify merges that distinct id with the new distinct id
+            expect(events[5].properties.distinct_id).to.eql('third-identify')
+            expect(events[5].properties['$anon_distinct_id']).to.eql(events[4].properties.distinct_id)
+            // then a final identified event includes that identified distinct id
+            expect(events[6].properties.distinct_id).to.eql('third-identify')
+        })
+    })
 })
