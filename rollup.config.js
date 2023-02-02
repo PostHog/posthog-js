57d70322821f08477d7647b544b8b73dd4c1ac38
Alex Kim <alexgkim205@gmail.com>
2023-02-02 13:43:27 -0500

    array.full.js rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import pkg from './package.json'
 import { terser } from 'rollup-plugin-terser'
+import copy from 'rollup-plugin-copy-merge'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 const plugins = [
@@ -19,6 +20,47 @@ const plugins = [
 ]
 
 export default [
+    {
+        input: 'src/loader-globals.ts',
+        output: [
+            {
+                file: 'dist/array.full.js',
+                sourcemap: true,
+                format: 'iife',
+                name: 'posthog',
+            },
+        ],
+        plugins: [
+            ...plugins,
+            copy({
+                targets: [
+                    {
+                        src: [
+                            'node_modules/rrweb/dist/record/rrweb-record.min.js',
+                            'node_modules/rrweb/dist/plugins/console-record.min.js',
+                        ],
+                        file: 'dist/recorder.js',
+                    },
+                    {
+                        src: 'node_modules/rrweb/dist/record/rrweb-record.min.js.map',
+                        dest: 'dist/',
+                        rename: 'recorder.js.map',
+                    },
+                    {
+                        src: 'node_modules/rrweb/dist/plugins/console-record.min.js.map',
+                        dest: 'dist/',
+                        rename: 'console-record.min.js.map',
+                    },
+                    {
+                        src: 'dist/recorder.js',
+                        dest: 'dist/',
+                        transform: (contents) => contents.toString().replace('rrweb-record.min.js.map', 'recorder.js.map')
+                    },
+                ],
+            }),
+            terser({ toplevel: true }),
+        ],
+    },
     {
         input: 'src/loader-globals.ts',
         output: [
