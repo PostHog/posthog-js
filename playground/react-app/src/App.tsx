81f0ad3948c51eab54f82f43d872e6daca388482
Luke Harries <lharries@users.noreply.github.com>
2023-02-16 15:49:37 +0000

    app

--- playground/react-app/src/App.tsx
+++ playground/react-app/src/App.tsx
@@ -3,11 +3,11 @@ import posthog from 'posthog-js'
 import Button from './Button'
 import { FeatureFlagGetter } from './FeatureFlagGetter'
 
-function App() {
-    posthog.init('phc_aIYpRzs2pU7hk93fqoTWtNezdeMUsNRCQFw7vnvDoOs', {
-        api_host: 'https://app.posthog.com',
-    })
+posthog.init('phc_aIYpRzs2pU7hk93fqoTWtNezdeMUsNRCQFw7vnvDoOs', {
+    api_host: 'https://app.posthog.com',
+})
 
+function App() {
     return (
         <PostHogProvider client={posthog}>
             <Button></Button>
