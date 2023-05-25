514543590a4a01a31df920e234e8bd491af5c356
Neil Kakkar <neilkakkar@gmail.com>
2023-05-25 12:51:24 +0100

    clean up

--- react/src/components/PostHogFeature.tsx
+++ react/src/components/PostHogFeature.tsx
@@ -1,7 +1,6 @@
 import { useFeatureFlagPayload, useFeatureFlagVariantKey, usePostHog } from '../hooks'
-import React, { RefObject, useEffect, useMemo, useRef, useState } from 'react';
+import React, { RefObject, useEffect, useMemo, useRef, useState } from 'react'
 import { PostHog } from '../context'
-
 
 export type PostHogFeatureProps = {
     flag: string
@@ -10,7 +9,12 @@ export type PostHogFeatureProps = {
     visibilityObserverOptions?: IntersectionObserverInit
 }
 
-export function PostHogFeature({ flag, match, children, visibilityObserverOptions }: PostHogFeatureProps): JSX.Element | null {
+export function PostHogFeature({
+    flag,
+    match,
+    children,
+    visibilityObserverOptions,
+}: PostHogFeatureProps): JSX.Element | null {
     const posthog = usePostHog()
     const payload = useFeatureFlagPayload(flag)
     const variant = useFeatureFlagVariantKey(flag)
@@ -19,13 +23,17 @@ export function PostHogFeature({ flag, match, children, visibilityObserverOption
     if (match === undefined || variant === match) {
         const childNode: React.ReactNode = typeof children === 'function' ? children(payload) : children
         return (
-            <div onClick={() => {
-              if (!clickTracked) {
-                trackClicks(flag, posthog)
-                setclickTracked(true)
-              }
-            }}>
-                <VisibilityTracker flag={flag} options={visibilityObserverOptions}>{childNode}</VisibilityTracker>
+            <div
+                onClick={() => {
+                    if (!clickTracked) {
+                        trackClicks(flag, posthog)
+                        setclickTracked(true)
+                    }
+                }}
+            >
+                <VisibilityTracker flag={flag} options={visibilityObserverOptions}>
+                    {childNode}
+                </VisibilityTracker>
             </div>
         )
     }
@@ -41,45 +49,46 @@ function trackVisibility(flag: string, posthog?: PostHog) {
     posthog?.capture('$feature_flag_viewed', { feature_flag: flag })
 }
 
-function VisibilityTracker({flag, children, options}: {flag: string, children: React.ReactNode, options?: IntersectionObserverInit}): JSX.Element {
-    const ref = useRef<HTMLDivElement>(null);
+function VisibilityTracker({
+    flag,
+    children,
+    options,
+}: {
+    flag: string
+    children: React.ReactNode
+    options?: IntersectionObserverInit
+}): JSX.Element {
+    const ref = useRef<HTMLDivElement>(null)
     const posthog = usePostHog()
     const [tracked, setTracked] = useState(false)
 
     const isIntersecting = useVisibleOnScreen(ref, {
         threshold: 0.1,
-        ...options
-    });
+        ...options,
+    })
 
     if (isIntersecting && !tracked) {
         trackVisibility(flag, posthog)
         setTracked(true)
     }
-  
-    return (
-      <div ref={ref}>
-        {children}
-      </div>
-    );
-};
-  
 
+    return <div ref={ref}>{children}</div>
+}
 
 const useVisibleOnScreen = (ref: RefObject<HTMLElement>, options?: IntersectionObserverInit) => {
-  const [isIntersecting, setIntersecting] = useState(false);
+    const [isIntersecting, setIntersecting] = useState(false)
 
-  const observer = useMemo(() => new IntersectionObserver(
-    ([entry]) => setIntersecting(entry.isIntersecting)
-  , options), [ref, options])
+    const observer = useMemo(
+        () => new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting), options),
+        [ref, options]
+    )
 
+    useEffect(() => {
+        if (ref.current === null) return
 
-  useEffect(() => {
-    if (ref.current === null) return
+        observer.observe(ref.current)
+        return () => observer.disconnect()
+    }, [ref])
 
-    observer.observe(ref.current)
-    return () => observer.disconnect()
-  }, [ref])
-
-  return isIntersecting
-};
-
+    return isIntersecting
+}
