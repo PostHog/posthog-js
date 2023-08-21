f942e32bec731d6501b84ead38127746af645148
Robbie Coomber <robbie.coomber@gmail.com>
2023-08-21 10:17:43 +0100

    Add retries

---
+++ scripts/run-testcafe-with-retries.mjs
@@ -0,0 +1,31 @@
+import yargs from "yargs"
+
+import {spawnSync} from "child_process"
+
+
+const main = async () => {
+  const argv= yargs(process.argv).argv
+  const attempts = argv.attempts || 3
+  const browser = argv.browser
+  if (!browser) {
+    throw new Error("Missing browser argument")
+  }
+
+
+  for (let i = 0; i < attempts; i++) {
+    console.log(`Attempt ${i + 1} of a maximum of ${attempts} attempts`)
+    const result = spawnSync("yarn", ["testcafe", browser], {stdio: "inherit"})
+    if (result.status === 0) {
+      console.log("Test succeeded")
+      return
+    }
+    console.log("Test failed")
+  }
+  throw new Error(`Test failed after ${retries} retries`)
+}
+
+
+main().catch(e => {
+  console.error(e)
+  process.exit(1)
+})
