a39d310d7a4a645dc7df529aca54e921bfdc7d8a
Robbie <robbie.coomber@gmail.com>
2023-08-22 07:24:48 +0100

    feat: Add pathname to prev page events (#776)
    * Add pathname to prev page events
    
    * Add a test for pathname
    
    * Change window import

--- src/page-view.ts
+++ src/page-view.ts
@@ -1,6 +1,7 @@
-import * as utils from './utils'
+import { window } from './utils'
 
 interface PageViewData {
+    pathname: string
     // scroll is how far down the page the user has scrolled,
     // content is how far down the page the user can view content
     // (e.g. if the page is 1000 tall, but the user's screen is only 500 tall,
@@ -24,15 +25,21 @@ interface ScrollProperties {
     $prev_pageview_max_content_percentage?: number
 }
 
+interface PageViewEventProperties extends ScrollProperties {
+    $prev_pageview_pathname?: string
+}
+
 export class PageViewManager {
     _pageViewData: PageViewData | undefined
     _hasSeenPageView = false
 
     _createPageViewData(): PageViewData {
-        return {}
+        return {
+            pathname: window.location.pathname,
+        }
     }
 
-    doPageView(): ScrollProperties {
+    doPageView(): PageViewEventProperties {
         let prevPageViewData: PageViewData | undefined
         // if there were events created before the first PageView, we would have created a
         // pageViewData for them. If this happened, we don't want to create a new pageViewData
@@ -51,12 +58,18 @@ export class PageViewManager {
         // of the event loop
         setTimeout(this._updateScrollData, 0)
 
-        return this._calculatePrevPageScrollProperties(prevPageViewData)
+        return {
+            $prev_pageview_pathname: prevPageViewData?.pathname,
+            ...this._calculatePrevPageScrollProperties(prevPageViewData),
+        }
     }
 
-    doPageLeave(): ScrollProperties {
+    doPageLeave(): PageViewEventProperties {
         const prevPageViewData = this._pageViewData
-        return this._calculatePrevPageScrollProperties(prevPageViewData)
+        return {
+            $prev_pageview_pathname: prevPageViewData?.pathname,
+            ...this._calculatePrevPageScrollProperties(prevPageViewData),
+        }
     }
 
     _calculatePrevPageScrollProperties(prevPageViewData: PageViewData | undefined): ScrollProperties {
@@ -121,34 +134,31 @@ export class PageViewManager {
     }
 
     startMeasuringScrollPosition() {
-        utils.window.addEventListener('scroll', this._updateScrollData)
-        utils.window.addEventListener('scrollend', this._updateScrollData)
-        utils.window.addEventListener('resize', this._updateScrollData)
+        window.addEventListener('scroll', this._updateScrollData)
+        window.addEventListener('scrollend', this._updateScrollData)
+        window.addEventListener('resize', this._updateScrollData)
     }
 
     stopMeasuringScrollPosition() {
-        utils.window.removeEventListener('scroll', this._updateScrollData)
-        utils.window.removeEventListener('scrollend', this._updateScrollData)
-        utils.window.removeEventListener('resize', this._updateScrollData)
+        window.removeEventListener('scroll', this._updateScrollData)
+        window.removeEventListener('scrollend', this._updateScrollData)
+        window.removeEventListener('resize', this._updateScrollData)
     }
 
     _scrollHeight(): number {
-        return Math.max(
-            0,
-            utils.window.document.documentElement.scrollHeight - utils.window.document.documentElement.clientHeight
-        )
+        return Math.max(0, window.document.documentElement.scrollHeight - window.document.documentElement.clientHeight)
     }
 
     _scrollY(): number {
-        return utils.window.scrollY || utils.window.pageYOffset || utils.window.document.documentElement.scrollTop || 0
+        return window.scrollY || window.pageYOffset || window.document.documentElement.scrollTop || 0
     }
 
     _contentHeight(): number {
-        return utils.window.document.documentElement.scrollHeight || 0
+        return window.document.documentElement.scrollHeight || 0
     }
 
     _contentY(): number {
-        const clientHeight = utils.window.document.documentElement.clientHeight || 0
+        const clientHeight = window.document.documentElement.clientHeight || 0
         return this._scrollY() + clientHeight
     }
 }
