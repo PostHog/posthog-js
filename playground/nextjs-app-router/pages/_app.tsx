414167dd8c67029777c5a7f33878a868d2823619
Pascal Pixel <x@pascalpixel.com>
2023-10-23 15:22:44 +0100

    make app router clone for nextjs

---
+++ playground/nextjs-app-router/pages/_app.tsx
@@ -0,0 +1,39 @@
+import '@/styles/globals.css'
+
+import React, { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+        session_recording: {
+            recordCrossOriginIframes: true,
+        },
+        debug: true,
+    })
+    ;(window as any).posthog = posthog
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+        // Track page views
+        const handleRouteChange = () => posthog.capture('$pageview')
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [])
+
+    return (
+        <PostHogProvider client={posthog}>
+            <Component {...pageProps} />
+        </PostHogProvider>
+    )
+}
---
+++ playground/nextjs-app-router/pages/index.tsx
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import { useFeatureFlagEnabled, usePostHog } from 'posthog-js/react'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+
+export default function Home() {
+    const posthog = usePostHog()
+    const result = useFeatureFlagEnabled('test')
+
+    const [time, setTime] = useState('')
+
+    useEffect(() => {
+        const t = setInterval(() => {
+            setTime(new Date().toISOString().split('T')[1].split('.')[0])
+        }, 1000)
+
+        return () => {
+            clearInterval(t)
+        }
+    }, [])
+
+    const randomID = () => Math.round(Math.random() * 10000)
+
+    return (
+        <>
+            <Head>
+                <title>PostHog</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <main>
+                <h1>PostHog React</h1>
+
+                <p>The current time is {time}</p>
+
+                <div className="buttons">
+                    <button onClick={() => posthog.capture('Clicked button')}>Capture event</button>
+                    <button data-attr="autocapture-button">Autocapture buttons</button>
+                    <a data-attr="autocapture-button" href="#">
+                        <span>Autocapture a &gt; span</span>
+                    </a>
+
+                    <button className="ph-no-capture">Ignore certain elements</button>
+
+                    <button onClick={() => posthog?.identify('user-' + randomID())}>Identify</button>
+
+                    <button
+                        onClick={() =>
+                            posthog?.setPersonProperties({
+                                email: `user-${randomID()}@posthog.com`,
+                            })
+                        }
+                    >
+                        Set user properties
+                    </button>
+                </div>
+
+                <div className="buttons">
+                    <Link href="/animations">Animations</Link>
+                    <Link href="/iframe">Iframe</Link>
+                    <Link href="/media">Media</Link>
+                    <Link href="/long">Long</Link>
+                </div>
+
+                <p>Feature flag response: {JSON.stringify(result)}</p>
+            </main>
+        </>
+    )
+}
