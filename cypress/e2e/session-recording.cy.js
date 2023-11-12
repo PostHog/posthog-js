8c3911b170e2e5aaaad570bf35be1870f0b91fbc
Paul D'Ambra <paul@posthog.com>
2023-11-12 18:55:36 +0000

    fix: more meaningful session recording integration tests

--- cypress/e2e/session-recording.cy.js
+++ cypress/e2e/session-recording.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { _isNull } from '../../src/utils/type-utils'
+
 describe('Session recording', () => {
     given('options', () => ({}))
 
@@ -26,7 +28,7 @@ describe('Session recording', () => {
             cy.wait('@decide')
         })
 
-        it('captures pageviews, autocapture, custom events', () => {
+        it('captures session events', () => {
             cy.get('[data-cy-input]').type('hello world! ')
             cy.wait(500)
             cy.get('[data-cy-input]')
@@ -74,7 +76,7 @@ describe('Session recording', () => {
             cy.wait('@recorder')
         })
 
-        it('captures pageviews, autocapture, custom events', () => {
+        it('captures session events', () => {
             cy.get('[data-cy-input]').type('hello world! ')
             cy.wait(500)
             cy.get('[data-cy-input]')
@@ -96,6 +98,61 @@ describe('Session recording', () => {
                         ).to.deep.equal(new Set([3]))
                     })
                 })
+        })
+
+        it('captures snapshots when the mouse moves', () => {
+            let sessionId = null
+
+            // cypress time handling can confuse when to run full snapshot, let's force that to happen...
+            cy.get('[data-cy-input]').type('hello world! ')
+            cy.wait('@session-recording').then(() => {
+                cy.phCaptures({ full: true }).then((captures) => {
+                    captures.forEach((c) => {
+                        if (_isNull(sessionId)) {
+                            sessionId = c.properties['$session_id']
+                        }
+                        // all captures should be from one session
+                        expect(c.properties['$session_id']).to.equal(sessionId)
+                    })
+                    expect(sessionId).not.to.be.null
+                })
+            })
+            // and then reset
+            cy.resetPhCaptures().then(() => {
+                cy.get('body')
+                    .trigger('mousemove', 50, 10)
+                    .trigger('mousemove', 52, 10)
+                    .trigger('mousemove', 54, 10)
+                    .trigger('mousemove', 56, 10)
+                cy.wait('@session-recording').then(() => {
+                    cy.phCaptures({ full: true }).then((captures) => {
+                        // should be a $snapshot for the current session
+                        expect(captures.map((c) => c.event)).to.deep.equal(['$snapshot'])
+                        expect(captures[0].properties['$session_id']).to.equal(sessionId)
+
+                        // the amount of captured data should be deterministic
+                        // but of course that would be too easy
+                        expect(captures[0]['properties']['$snapshot_data']).to.have.length.above(0)
+
+                        /**
+                         * the snapshots will look a little like:
+                         * [
+                         *  {"type":3,"data":{"source":6,"positions":[{"x":58,"y":18,"id":15,"timeOffset":0}]},"timestamp":1699814887222},
+                         *  {"type":3,"data":{"source":6,"positions":[{"x":58,"y":18,"id":15,"timeOffset":-430}]},"timestamp":1699814887722}
+                         *  ]
+                         */
+
+                        const xPositions = []
+                        for (let i = 0; i < captures[0]['properties']['$snapshot_data'].length; i++) {
+                            expect(captures[0]['properties']['$snapshot_data'][i].type).to.equal(3)
+                            expect(captures[0]['properties']['$snapshot_data'][i].data.source).to.equal(6)
+                            xPositions.push(captures[0]['properties']['$snapshot_data'][i].data.positions[0].x)
+                        }
+
+                        expect(xPositions).to.eql([58, 64])
+                    })
+                })
+            })
         })
     })
 })
