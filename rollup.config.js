3d8583209626a2a4d3919eedea535a7790bb856b
Li Yi <li@posthog.com>
2024-01-16 21:39:58 -0500

    add preset env types back in

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ const plugins = [
     babel({
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
         babelHelpers: 'bundled',
+        presets: ['@babel/preset-env'],
     }),
     terser({ toplevel: true }),
     visualizer(),
