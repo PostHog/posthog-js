9fef5f642248ed2a66d2b1090211e18f7aa2e049
Paul D'Ambra <paul@posthog.com>
2024-01-22 12:21:05 +0000

    fix: integration test opting out (#959)
    * fix: integration test opting out
    
    * opting out means we don't download recorder.js
    
    * probably working but tests not passing
    
    * some reorganization
    
    * more reorganising and removal of given
    
    * continue converting to typescript
    
    * more ts conversion

--- src/extensions/replay/sessionrecording.ts
+++ src/extensions/replay/sessionrecording.ts
@@ -25,6 +25,7 @@ import { logger } from '../../utils/logger'
 import { assignableWindow, window } from '../../utils/globals'
 import { buildNetworkRequestOptions } from './config'
 import { isLocalhost } from '../../utils/request-utils'
+import { userOptedOut } from '../../gdpr-utils'
 
 const BASE_ENDPOINT = '/s/'
 
@@ -364,7 +365,8 @@ export class SessionRecording {
         }
 
         // We do not switch recorder versions midway through a recording.
-        if (this._captureStarted || this.instance.config.disable_session_recording) {
+        // do not start if explicitly disabled or if the user has opted out
+        if (this._captureStarted || this.instance.config.disable_session_recording || userOptedOut(this.instance)) {
             return
         }
 
