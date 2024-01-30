733cf0fb73fcf2765a675740a82460c510929bee
Li Yi <li@posthog.com>
2024-01-30 17:17:40 -0500

    change order of capture wait

--- cypress/e2e/surveys.cy.ts
+++ cypress/e2e/surveys.cy.ts
@@ -579,8 +579,8 @@ describe('Surveys', () => {
             cy.intercept('POST', '**/e/*').as('capture-assertion')
             onPageLoad()
             // first capture is $pageview
-            // cy.wait('@capture-assertion')
             cy.get('.PostHogSurvey123').shadow().find('.cancel-btn-wrapper').click()
+            cy.wait('@capture-assertion')
             cy.wait('@capture-assertion').then(async ({ request }) => {
                 const captures = await getBase64EncodedPayload(request)
                 expect(captures.map(({ event }) => event)).to.deep.equal([
