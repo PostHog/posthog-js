4ba4bebe0087e07bdfa10b5854edffc5d8d17510
Li Yi <li@posthog.com>
2024-02-07 19:13:47 -0500

    only export preview render

--- src/loader-module.ts
+++ src/loader-module.ts
@@ -2,6 +2,6 @@ import { init_as_module } from './posthog-core'
 export { PostHog } from './posthog-core'
 export * from './types'
 export * from './posthog-surveys-types'
-export * from './extensions/surveys'
+export { renderSurveysPreview } from './extensions/surveys'
 export const posthog = init_as_module()
 export default posthog
