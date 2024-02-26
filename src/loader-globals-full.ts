c75a997700b5cb405af3924ce73f7eb853a47385
Neil Kakkar <neilkakkar@gmail.com>
2024-02-26 17:20:06 +0000

    feat(surveys): Load surveys in array.full.js

--- src/loader-globals-full.ts
+++ src/loader-globals-full.ts
@@ -1,6 +1,7 @@
-// Same as loader-globals.ts except includes rrweb scripts.
+// Same as loader-globals.ts except includes rrweb and surveys scripts.
 
 import './loader-recorder'
+import './loader-surveys'
 import { init_from_snippet } from './posthog-core'
 
 init_from_snippet()
