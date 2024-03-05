c26dd05b519698d1b8d0e7b3d6a8c7a9e5f671f8
Ben White <ben@posthog.com>
2024-03-05 09:15:32 +0100

    Tidying

--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -13,49 +13,44 @@ export class RateLimiter {
     instance: PostHog
     serverLimits: Record<string, number> = {}
 
-    tokensPerSecond: number
-    burst: number
+    captureEventsPerSecond: number
+    captureEventsBurstLimit: number
 
     constructor(instance: PostHog) {
         this.instance = instance
 
-        this.tokensPerSecond = instance.config.rate_limiting?.events_per_second || 10
-        this.burst = Math.max(
-            instance.config.rate_limiting?.events_burst_limit || this.tokensPerSecond * 10,
-            this.tokensPerSecond
+        this.captureEventsPerSecond = instance.config.rate_limiting?.events_per_second || 10
+        this.captureEventsBurstLimit = Math.max(
+            instance.config.rate_limiting?.events_burst_limit || this.captureEventsPerSecond * 10,
+            this.captureEventsPerSecond
         )
     }
 
     public isCaptureRateLimited(checkOnly = false): boolean {
         // This is primarily to prevent runaway loops from flooding capture with millions of events for a single user.
         // It's as much for our protection as theirs.
-
-        const bucket = this.instance.persistence?.get_property(CAPTURE_RATE_LIMIT) ?? {
-            tokens: this.burst,
-            last: new Date().getTime(),
-        }
-
         const now = new Date().getTime()
-        const elapsed = now - bucket.last
-        const tokensToAdd = elapsed * (this.tokensPerSecond / 1000)
+        const bucket = this.instance.persistence?.get_property(CAPTURE_RATE_LIMIT) ?? {
+            tokens: this.captureEventsBurstLimit,
+            last: now,
+        }
+
+        bucket.tokens += ((now - bucket.last) / 1000) * this.captureEventsPerSecond
         bucket.last = now
-        bucket.tokens += tokensToAdd
 
-        if (bucket.tokens > this.burst) {
-            bucket.tokens = this.burst
+        if (bucket.tokens > this.captureEventsBurstLimit) {
+            bucket.tokens = this.captureEventsBurstLimit
         }
 
-        if (bucket.tokens < 1) {
-            return true
-        }
+        const isRateLimited = bucket.tokens < 1
 
-        if (!checkOnly) {
+        if (!isRateLimited && !checkOnly) {
             bucket.tokens = Math.max(0, bucket.tokens - 1)
         }
 
         this.instance.persistence?.set_property(CAPTURE_RATE_LIMIT, bucket)
 
-        return false
+        return isRateLimited
     }
 
     public isServerRateLimited(batchKey: string | undefined): boolean {
