427499d91661e5e3bbd4393235a28594ef141dd3
Ben White <ben@posthog.com>
2024-03-18 09:45:21 +0100

    Fix tests

--- src/__tests__/posthog-core.js
+++ src/__tests__/posthog-core.js
@@ -1125,16 +1125,16 @@ describe('posthog core', () => {
         })
 
         it('returns the replay URL', () => {
-            expect(given.lib.get_session_replay_url()).toEqual('https://app.posthog.com/replay/sessionId')
+            expect(given.lib.get_session_replay_url()).toEqual('https://us.posthog.com/replay/sessionId')
         })
 
         it('returns the replay URL including timestamp', () => {
             expect(given.lib.get_session_replay_url({ withTimestamp: true })).toEqual(
-                'https://app.posthog.com/replay/sessionId?t=20' // default lookback is 10 seconds
+                'https://us.posthog.com/replay/sessionId?t=20' // default lookback is 10 seconds
             )
 
             expect(given.lib.get_session_replay_url({ withTimestamp: true, timestampLookBack: 0 })).toEqual(
-                'https://app.posthog.com/replay/sessionId?t=30'
+                'https://us.posthog.com/replay/sessionId?t=30'
             )
         })
     })
