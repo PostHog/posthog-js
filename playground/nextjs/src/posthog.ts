bfde411594ac80b6deebdddc756b2d7e2e1b030e
Ben White <ben@posthog.com>
2024-04-23 15:49:23 +0200

    Deploy it

--- playground/nextjs/src/posthog.ts
+++ playground/nextjs/src/posthog.ts
@@ -37,6 +37,7 @@ export const updatePostHogConsent = (consentGiven: boolean) => {
     posthog.set_config(configForConsent())
 }
 
+
 if (typeof window !== 'undefined') {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
