92310b441c992734c19ad88e3b78288ce9bfc06b
Ben White <ben@posthog.com>
2024-04-24 13:00:19 +0200

    Fix

--- playground/nextjs/pages/_document.tsx
+++ playground/nextjs/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
 export default function Document() {
     return (
         <Html lang="en">
+            <Head />
+
             <body>
                 <Main />
             </body>
