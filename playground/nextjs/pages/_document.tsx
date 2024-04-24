67b6837f0ce04ad163e37979728e941e8a73ecc4
Ben White <ben@posthog.com>
2024-04-24 12:22:20 +0200

    fix

--- playground/nextjs/pages/_document.tsx
+++ playground/nextjs/pages/_document.tsx
@@ -1,14 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
 import React from 'react'
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <title>PostHog</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-
             <body>
                 <Main />
             </body>
