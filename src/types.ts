5f17bddffd3caaa9144b0f2acd99140e516828df
Paul D'Ambra <paul@posthog.com>
2024-06-24 11:54:15 +0100

    first pass moving to having a level on errors too

--- src/types.ts
+++ src/types.ts
@@ -473,9 +473,16 @@ export type ErrorEventArgs = [
     error?: Error | undefined
 ]
 
+// levels originally copied from Sentry to work with the sentry integration
+// and to avoid relying on a frequently changing @sentry/types dependency
+// but provided as an array of literal types, so we can constrain the level below
+export const severityLevels = ['fatal', 'error', 'warning', 'log', 'info', 'debug'] as const
+export declare type SeverityLevel = (typeof severityLevels)[number]
+
 export interface ErrorProperties {
     $exception_type: string
     $exception_message: string
+    $exception_level: SeverityLevel
     $exception_source?: string
     $exception_lineno?: number
     $exception_colno?: number
