7179f11905ceaed98064cb19e11ece6a189b01a7
Paul D'Ambra <paul@posthog.com>
2024-07-02 17:53:27 +0100

    like this

--- src/entrypoints/surveys.ts
+++ src/entrypoints/surveys.ts
@@ -1,5 +1,6 @@
 import { generateSurveys } from '../extensions/surveys'
 import { SurveyEventReceiver } from '../extensions/surveys/survey-event-receiver'
+import { canActivateRepeatedly, hasEvents } from './extensions/surveys/surveys-utils'
 
 import { window } from '../utils/globals'
 
@@ -7,6 +8,8 @@ if (window) {
     ;(window as any).__PosthogExtensions__ = (window as any).__Posthog__ || {}
     ;(window as any).extendPostHogWithSurveys = generateSurveys
     ;(window as any).__PosthogExtensions__.SurveysEventReceiver = SurveyEventReceiver
+    ;(window as any).__PosthogExtensions__.canActivateRepeatedly = canActivateRepeatedly
+    ;(window as any).__PosthogExtensions__.hasEvents = hasEvents
 }
 
 export default generateSurveys
--- src/posthog-surveys.ts
+++ src/posthog-surveys.ts
@@ -12,7 +12,6 @@ import { assignableWindow, document, window } from './utils/globals'
 import { CaptureResult, DecideResponse, IReceiveSurveyEvents } from './types'
 import { logger } from './utils/logger'
 import { isUndefined } from './utils/type-utils'
-import { canActivateRepeatedly, hasEvents } from './extensions/surveys/surveys-utils'
 
 export const surveyUrlValidationMap: Record<SurveyUrlMatchType, (conditionsUrl: string) => boolean> = {
     icontains: (conditionsUrl) =>
@@ -169,9 +168,12 @@ export class PostHogSurveys {
                     ? this.instance.featureFlags.isFeatureEnabled(survey.targeting_flag_key)
                     : true
 
-                const eventBasedTargetingFlagCheck = hasEvents(survey) ? activatedSurveys?.includes(survey.id) : true
+                const eventBasedTargetingFlagCheck = assignableWindow.__PosthogExtensions__.hasEvents(survey)
+                    ? activatedSurveys?.includes(survey.id)
+                    : true
 
-                const overrideInternalTargetingFlagCheck = canActivateRepeatedly(survey)
+                const overrideInternalTargetingFlagCheck =
+                    assignableWindow.__PosthogExtensions__.canActivateRepeatedly(survey)
                 const internalTargetingFlagCheck =
                     survey.internal_targeting_flag_key && !overrideInternalTargetingFlagCheck
                         ? this.instance.featureFlags.isFeatureEnabled(survey.internal_targeting_flag_key)
