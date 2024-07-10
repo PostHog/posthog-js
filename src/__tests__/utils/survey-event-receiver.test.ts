ec00d33f793359b416af60f99c25bbab5f9f169c
Phani Raj <phani@posthog.com>
2024-07-10 10:48:26 -0500

    feat(Surveys): Activate surveys based on actions (#1295)
    This commit allows us to activate surveys if any actions they're configured with are performed in the browser.

--- src/__tests__/utils/survey-event-receiver.test.ts
+++ src/__tests__/utils/survey-event-receiver.test.ts
@@ -1,157 +1,321 @@
 /// <reference lib="dom" />
 
-import { SurveyType, SurveyQuestionType, Survey } from '../../posthog-surveys-types'
+import {
+    SurveyType,
+    SurveyQuestionType,
+    Survey,
+    ActionType,
+    ActionStepStringMatching,
+} from '../../posthog-surveys-types'
 import { PostHogPersistence } from '../../posthog-persistence'
 import { PostHog } from '../../posthog-core'
-import { PostHogConfig } from '../../types'
+import { CaptureResult, PostHogConfig } from '../../types'
 import { SurveyEventReceiver } from '../../utils/survey-event-receiver'
 
 describe('survey-event-receiver', () => {
-    let config: PostHogConfig
-    let instance: PostHog
+    describe('event based surveys', () => {
+        let config: PostHogConfig
+        let instance: PostHog
 
-    const surveysWithEvents: Survey[] = [
-        {
+        const surveysWithEvents: Survey[] = [
+            {
+                name: 'first survey',
+                id: 'first-survey',
+                description: 'first survey description',
+                type: SurveyType.Popover,
+                questions: [{ type: SurveyQuestionType.Open, question: 'what is a bokoblin?' }],
+                conditions: {
+                    events: {
+                        values: [
+                            {
+                                name: 'user_subscribed',
+                            },
+                            {
+                                name: 'user_unsubscribed',
+                            },
+                            {
+                                name: 'billing_changed',
+                            },
+                            {
+                                name: 'billing_removed',
+                            },
+                        ],
+                    },
+                },
+            } as unknown as Survey,
+            {
+                name: 'second survey',
+                id: 'second-survey',
+                description: 'second survey description',
+                type: SurveyType.Popover,
+                questions: [{ type: SurveyQuestionType.Open, question: 'what is a moblin?' }],
+            } as unknown as Survey,
+            {
+                name: 'third survey',
+                id: 'third-survey',
+                description: 'third survey description',
+                type: SurveyType.Popover,
+                questions: [{ type: SurveyQuestionType.Open, question: 'what is a bokoblin?' }],
+                conditions: {
+                    events: {
+                        values: [
+                            {
+                                name: 'user_subscribed',
+                            },
+                            {
+                                name: 'user_unsubscribed',
+                            },
+                            {
+                                name: 'address_changed',
+                            },
+                        ],
+                    },
+                },
+            } as unknown as Survey,
+        ]
+
+        beforeEach(() => {
+            config = {
+                token: 'testtoken',
+                api_host: 'https://app.posthog.com',
+                persistence: 'memory',
+            } as unknown as PostHogConfig
+
+            instance = {
+                config: config,
+                persistence: new PostHogPersistence(config),
+                _addCaptureHook: jest.fn(),
+            } as unknown as PostHog
+        })
+
+        afterEach(() => {
+            instance.persistence?.clear()
+        })
+
+        it('register makes receiver listen for all surveys with events', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            const registry = surveyEventReceiver.getEventToSurveys()
+            expect(registry.has('user_subscribed')).toBeTruthy()
+            expect(registry.get('user_subscribed')).toEqual(['first-survey', 'third-survey'])
+
+            expect(registry.has('address_changed')).toBeTruthy()
+            expect(registry.get('address_changed')).toEqual(['third-survey'])
+        })
+
+        it('receiver activates survey on event', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            surveyEventReceiver.onEvent('billing_changed')
+            const activatedSurveys = surveyEventReceiver.getSurveys()
+            expect(activatedSurveys).toContain('first-survey')
+        })
+
+        it('receiver removes survey from list after its shown', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            surveyEventReceiver.onEvent('billing_changed')
+            const activatedSurveys = surveyEventReceiver.getSurveys()
+            expect(activatedSurveys).toContain('first-survey')
+
+            surveyEventReceiver.onEvent('survey shown', {
+                $set: undefined,
+                $set_once: undefined,
+                event: 'survey shown',
+                timestamp: undefined,
+                uuid: '',
+                properties: {
+                    $survey_id: 'first-survey',
+                },
+            })
+
+            expect(surveyEventReceiver.getSurveys()).toEqual([])
+        })
+
+        it('receiver activates same survey on multiple event', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            surveyEventReceiver.onEvent('billing_changed')
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+            surveyEventReceiver.onEvent('billing_removed')
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+        })
+
+        it('receiver activates multiple surveys on same event', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            surveyEventReceiver.onEvent('user_subscribed')
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey', 'third-survey'])
+        })
+
+        it('receiver activates multiple surveys on different events', () => {
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register(surveysWithEvents)
+            surveyEventReceiver.onEvent('billing_changed')
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+            surveyEventReceiver.onEvent('address_changed')
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey', 'third-survey'])
+        })
+    })
+
+    describe('action based surveys', () => {
+        let config: PostHogConfig
+        let instance: PostHog
+
+        beforeEach(() => {
+            config = {
+                token: 'testtoken',
+                api_host: 'https://app.posthog.com',
+                persistence: 'memory',
+            } as unknown as PostHogConfig
+
+            instance = {
+                config: config,
+                persistence: new PostHogPersistence(config),
+                _addCaptureHook: jest.fn(),
+            } as unknown as PostHog
+        })
+
+        afterEach(() => {
+            instance.persistence?.clear()
+        })
+
+        const createCaptureResult = (eventName: string, currentUrl?: string): CaptureResult => {
+            return {
+                $set: undefined,
+                $set_once: undefined,
+                properties: {
+                    $current_url: currentUrl,
+                },
+                timestamp: undefined,
+                uuid: '0C984DA5-761F-4F75-9582-D2F95B43B04A',
+                event: eventName,
+            }
+        }
+        const createAction = (
+            id: number,
+            eventName: string,
+            currentUrl?: string,
+            urlMatch?: ActionStepStringMatching
+        ): ActionType => {
+            return {
+                id: id,
+                name: `${eventName || 'user defined '} action`,
+                description: '',
+                post_to_slack: false,
+                slack_message_format: '',
+                steps: [
+                    {
+                        event: eventName,
+                        properties: null,
+                        text: null,
+                        text_matching: null,
+                        href: null,
+                        href_matching: null,
+                        url: currentUrl,
+                        url_matching: urlMatch || 'exact',
+                    },
+                ],
+                created_at: '2024-06-20T14:39:23.616676Z',
+                deleted: false,
+                is_calculating: false,
+                last_calculated_at: '2024-06-20T14:39:23.616051Z',
+                is_action: true,
+                bytecode_error: null,
+                tags: [],
+            }
+        }
+
+        const autoCaptureSurvey = {
             name: 'first survey',
             id: 'first-survey',
             description: 'first survey description',
             type: SurveyType.Popover,
             questions: [{ type: SurveyQuestionType.Open, question: 'what is a bokoblin?' }],
             conditions: {
-                events: {
-                    values: [
-                        {
-                            name: 'user_subscribed',
-                        },
-                        {
-                            name: 'user_unsubscribed',
-                        },
-                        {
-                            name: 'billing_changed',
-                        },
-                        {
-                            name: 'billing_removed',
-                        },
-                    ],
-                },
+                actions: [createAction(2, '$autocapture') as unknown as ActionType],
             },
-        } as unknown as Survey,
-        {
-            name: 'second survey',
-            id: 'second-survey',
-            description: 'second survey description',
-            type: SurveyType.Popover,
-            questions: [{ type: SurveyQuestionType.Open, question: 'what is a moblin?' }],
-        } as unknown as Survey,
-        {
-            name: 'third survey',
-            id: 'third-survey',
-            description: 'third survey description',
+        } as unknown as Survey
+
+        const pageViewSurvey = {
+            name: 'pageview survey',
+            id: 'pageview-survey',
+            description: 'pageview survey description',
             type: SurveyType.Popover,
             questions: [{ type: SurveyQuestionType.Open, question: 'what is a bokoblin?' }],
             conditions: {
-                events: {
-                    values: [
-                        {
-                            name: 'user_subscribed',
-                        },
-                        {
-                            name: 'user_unsubscribed',
-                        },
-                        {
-                            name: 'address_changed',
-                        },
-                    ],
+                actions: [createAction(3, '$pageview') as unknown as ActionType],
+            },
+        } as unknown as Survey
+
+        it('can match action on event name', () => {
+            const myPageViewSurvey = {
+                name: 'my pageview survey',
+                id: 'my-pageview-survey',
+                description: 'pageview survey description',
+                type: SurveyType.Popover,
+                questions: [{ type: SurveyQuestionType.Open, question: 'what is a bokoblin?' }],
+                conditions: {
+                    actions: {
+                        values: [createAction(3, '$mypageview') as unknown as ActionType],
+                    },
                 },
-            },
-        } as unknown as Survey,
-    ]
+            } as unknown as Survey
+            autoCaptureSurvey.conditions.actions.values = [createAction(2, '$match_event_name')]
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register([autoCaptureSurvey, myPageViewSurvey])
+            surveyEventReceiver._getActionMatcher().on('$match_event_name', createCaptureResult('$match_event_name'))
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
 
-    beforeEach(() => {
-        config = {
-            token: 'testtoken',
-            api_host: 'https://app.posthog.com',
-            persistence: 'memory',
-        } as unknown as PostHogConfig
-
-        instance = {
-            config: config,
-            persistence: new PostHogPersistence(config),
-        } as unknown as PostHog
-    })
-
-    afterEach(() => {
-        instance.persistence?.clear()
-    })
-
-    it('register makes receiver listen for all surveys with events', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        const registry = surveyEventReceiver.getEventRegistry()
-        expect(registry.has('second-survey')).toBeFalsy()
-        expect(registry.has('first-survey')).toBeTruthy()
-        expect(registry.get('first-survey')).toEqual([
-            'user_subscribed',
-            'user_unsubscribed',
-            'billing_changed',
-            'billing_removed',
-        ])
-
-        expect(registry.has('third-survey')).toBeTruthy()
-        expect(registry.get('third-survey')).toEqual(['user_subscribed', 'user_unsubscribed', 'address_changed'])
-    })
-
-    it('receiver activates survey on event', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        surveyEventReceiver.on('billing_changed')
-        const activatedSurveys = surveyEventReceiver.getSurveys()
-        expect(activatedSurveys).toContain('first-survey')
-    })
-
-    it('receiver removes survey from list after its shown', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        surveyEventReceiver.on('billing_changed')
-        const activatedSurveys = surveyEventReceiver.getSurveys()
-        expect(activatedSurveys).toContain('first-survey')
-
-        surveyEventReceiver.on('survey shown', {
-            $set: undefined,
-            $set_once: undefined,
-            event: 'survey shown',
-            timestamp: undefined,
-            uuid: '',
-            properties: {
-                $survey_id: 'first-survey',
-            },
+            surveyEventReceiver
+                ._getActionMatcher()
+                .on('$mypageview', createCaptureResult(myPageViewSurvey.conditions.actions.values[0].steps[0].event))
+            expect(surveyEventReceiver.getSurveys()).toContain('my-pageview-survey')
         })
 
-        expect(surveyEventReceiver.getSurveys()).toEqual([])
-    })
+        it('can match action on current_url exact', () => {
+            autoCaptureSurvey.conditions.actions.values = [createAction(2, '$autocapture', 'https://us.posthog.com')]
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register([autoCaptureSurvey, pageViewSurvey])
+            surveyEventReceiver
+                ._getActionMatcher()
+                .on('$autocapture', createCaptureResult('$autocapture', 'https://eu.posthog.com'))
+            expect(surveyEventReceiver.getSurveys()).not.toEqual(['first-survey'])
+            surveyEventReceiver
+                ._getActionMatcher()
+                .on('$autocapture', createCaptureResult('$autocapture', 'https://us.posthog.com'))
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+        })
 
-    it('receiver activates same survey on multiple event', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        surveyEventReceiver.on('billing_changed')
-        expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
-        surveyEventReceiver.on('billing_removed')
-        expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
-    })
+        it('can match action on current_url regexp', () => {
+            autoCaptureSurvey.conditions.actions.values = [
+                createAction(2, '$current_url_regexp', '[a-z][a-z].posthog.*', 'regex'),
+            ]
+            let surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register([autoCaptureSurvey, pageViewSurvey])
+            surveyEventReceiver
+                ._getActionMatcher()
+                .on('$autocapture', createCaptureResult('$current_url_regexp', 'https://eu.posthog.com'))
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
 
-    it('receiver activates multiple surveys on same event', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        surveyEventReceiver.on('user_subscribed')
-        expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey', 'third-survey'])
-    })
+            surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register([autoCaptureSurvey, pageViewSurvey])
+            surveyEventReceiver
+                ._getActionMatcher()
+                .on('$autocapture', createCaptureResult('$autocapture', 'https://us.posthog.com'))
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+        })
 
-    it('receiver activates multiple surveys on different events', () => {
-        const surveyEventReceiver = new SurveyEventReceiver(instance.persistence)
-        surveyEventReceiver.register(surveysWithEvents)
-        surveyEventReceiver.on('billing_changed')
-        expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
-        surveyEventReceiver.on('address_changed')
-        expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey', 'third-survey'])
+        it('can match action on html element selector', () => {
+            const action = createAction(2, '$autocapture')
+            action.steps[0].selector = '* > #__next .flex > button:nth-child(2)'
+            autoCaptureSurvey.conditions.actions.values = [action]
+            const result = createCaptureResult('$autocapture', 'https://eu.posthog.com')
+            result.properties.$element_selectors = ['* > #__next .flex > button:nth-child(2)']
+            const surveyEventReceiver = new SurveyEventReceiver(instance)
+            surveyEventReceiver.register([autoCaptureSurvey, pageViewSurvey])
+            surveyEventReceiver._getActionMatcher().on('$autocapture', result)
+            expect(surveyEventReceiver.getSurveys()).toEqual(['first-survey'])
+        })
     })
 })
--- src/utils/survey-event-receiver.ts
+++ src/utils/survey-event-receiver.ts
@@ -1,37 +1,115 @@
 import { Survey } from '../posthog-surveys-types'
-import { PostHogPersistence } from '../posthog-persistence'
 import { SURVEYS_ACTIVATED } from '../constants'
+
 import { CaptureResult } from '../types'
+import { ActionMatcher } from '../extensions/surveys/action-matcher'
+import { PostHog } from '../posthog-core'
+import { isUndefined } from './type-utils'
 
 export class SurveyEventReceiver {
-    private readonly eventRegistry: Map<string, string[]>
-    private readonly persistence?: PostHogPersistence
+    // eventToSurveys is a mapping of event name to all the surveys that are activated by it
+    private readonly eventToSurveys: Map<string, string[]>
+    // actionToSurveys is a mapping of action name to all the surveys that are activated by it
+    private readonly actionToSurveys: Map<string, string[]>
+    // actionMatcher can look at CaptureResult payloads and match an event to its corresponding action.
+    private actionMatcher?: ActionMatcher | null
+    private readonly instance?: PostHog
     private static SURVEY_SHOWN_EVENT_NAME = 'survey shown'
 
-    constructor(persistence?: PostHogPersistence) {
-        this.persistence = persistence
-        this.eventRegistry = new Map<string, string[]>()
+    constructor(instance: PostHog) {
+        this.instance = instance
+        this.eventToSurveys = new Map<string, string[]>()
+        this.actionToSurveys = new Map<string, string[]>()
     }
 
     register(surveys: Survey[]): void {
-        surveys.forEach((survey) => {
+        if (isUndefined(this.instance?._addCaptureHook)) {
+            return
+        }
+
+        this.setupEventBasedSurveys(surveys)
+        this.setupActionBasedSurveys(surveys)
+    }
+
+    private setupActionBasedSurveys(surveys: Survey[]) {
+        const actionBasedSurveys = surveys.filter(
+            (survey: Survey) => survey.conditions?.actions && survey.conditions?.actions?.values?.length > 0
+        )
+
+        if (actionBasedSurveys.length === 0) {
+            return
+        }
+
+        if (this.actionMatcher == null) {
+            this.actionMatcher = new ActionMatcher(this.instance)
+            this.actionMatcher.init()
+            // match any actions to its corresponding survey.
+            const matchActionToSurvey = (actionName: string) => {
+                this.onAction(actionName)
+            }
+
+            this.actionMatcher._addActionHook(matchActionToSurvey)
+        }
+
+        actionBasedSurveys.forEach((survey) => {
             if (
-                survey.conditions?.events &&
-                survey.conditions?.events?.values &&
-                survey.conditions?.events.values.length > 0
+                survey.conditions &&
+                survey.conditions?.actions &&
+                survey.conditions?.actions?.values &&
+                survey.conditions?.actions?.values?.length > 0
             ) {
-                this.eventRegistry.set(
-                    survey.id,
-                    survey.conditions?.events.values.map((e) => e.name)
-                )
+                // register the known set of actions with
+                // the action-matcher so it can match
+                // events to actions
+                this.actionMatcher?.register(survey.conditions.actions.values)
+
+                // maintain a mapping of (Action1) => [Survey1, Survey2, Survey3]
+                // where Surveys 1-3 are all activated by Action1
+                survey.conditions?.actions?.values?.forEach((action) => {
+                    if (action && action.name) {
+                        const knownSurveys: string[] | undefined = this.actionToSurveys.get(action.name)
+                        if (knownSurveys) {
+                            knownSurveys.push(survey.id)
+                        }
+                        this.actionToSurveys.set(action.name, knownSurveys || [survey.id])
+                    }
+                })
             }
         })
     }
 
-    on(event: string, eventPayload?: CaptureResult): void {
-        const activatedSurveys: string[] = []
-        const existingActivatedSurveys: string[] = this.persistence?.props[SURVEYS_ACTIVATED] || []
+    private setupEventBasedSurveys(surveys: Survey[]) {
+        const eventBasedSurveys = surveys.filter(
+            (survey: Survey) => survey.conditions?.events && survey.conditions?.events?.values?.length > 0
+        )
 
+        if (eventBasedSurveys.length === 0) {
+            return
+        }
+
+        // match any events to its corresponding survey.
+        const matchEventToSurvey = (eventName: string) => {
+            this.onEvent(eventName)
+        }
+        this.instance?._addCaptureHook(matchEventToSurvey)
+
+        surveys.forEach((survey) => {
+            // maintain a mapping of (Event1) => [Survey1, Survey2, Survey3]
+            // where Surveys 1-3 are all activated by Event1
+            survey.conditions?.events?.values?.forEach((event) => {
+                if (event && event.name) {
+                    const knownSurveys: string[] | undefined = this.eventToSurveys.get(event.name)
+                    if (knownSurveys) {
+                        knownSurveys.push(survey.id)
+                    }
+                    this.eventToSurveys.set(event.name, knownSurveys || [survey.id])
+                }
+            })
+        })
+    }
+
+    onEvent(event: string, eventPayload?: CaptureResult): void {
+        const existingActivatedSurveys: string[] = this.instance?.persistence?.props[SURVEYS_ACTIVATED] || []
         if (
             SurveyEventReceiver.SURVEY_SHOWN_EVENT_NAME == event &&
             eventPayload &&
@@ -43,33 +121,40 @@ export class SurveyEventReceiver {
                 const index = existingActivatedSurveys.indexOf(surveyId)
                 if (index >= 0) {
                     existingActivatedSurveys.splice(index, 1)
+                    this._updateActivatedSurveys(existingActivatedSurveys)
                 }
             }
         } else {
-            this.eventRegistry.forEach((events, surveyID) => {
-                if (events.includes(event)) {
-                    activatedSurveys.push(surveyID)
-                }
-            })
+            if (this.eventToSurveys.has(event)) {
+                this._updateActivatedSurveys(existingActivatedSurveys.concat(this.eventToSurveys.get(event) || []))
+            }
         }
+    }
 
-        const updatedSurveys = existingActivatedSurveys.concat(activatedSurveys)
-        this._saveSurveysToStorage(updatedSurveys)
+    onAction(actionName: string): void {
+        const existingActivatedSurveys: string[] = this.instance?.persistence?.props[SURVEYS_ACTIVATED] || []
+        if (this.actionToSurveys.has(actionName)) {
+            this._updateActivatedSurveys(existingActivatedSurveys.concat(this.actionToSurveys.get(actionName) || []))
+        }
+    }
+
+    private _updateActivatedSurveys(activatedSurveys: string[]) {
+        // we use a new Set here to remove duplicates.
+        this.instance?.persistence?.register({
+            [SURVEYS_ACTIVATED]: [...new Set(activatedSurveys)],
+        })
     }
 
     getSurveys(): string[] {
-        const existingActivatedSurveys = this.persistence?.props[SURVEYS_ACTIVATED]
+        const existingActivatedSurveys = this.instance?.persistence?.props[SURVEYS_ACTIVATED]
         return existingActivatedSurveys ? existingActivatedSurveys : []
     }
 
-    getEventRegistry(): Map<string, string[]> {
-        return this.eventRegistry
+    getEventToSurveys(): Map<string, string[]> {
+        return this.eventToSurveys
     }
 
-    private _saveSurveysToStorage(surveys: string[]): void {
-        // we use a new Set here to remove duplicates.
-        this.persistence?.register({
-            [SURVEYS_ACTIVATED]: [...new Set(surveys)],
-        })
+    _getActionMatcher(): ActionMatcher | null | undefined {
+        return this.actionMatcher
     }
 }
