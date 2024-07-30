34ca27f9c82933f7b97bceefd4022b7649874baf
dylan <dylan@posthog.com>
2024-07-30 12:57:19 -0700

    fix up the react app

--- playground/nextjs/pages/survey.tsx
+++ playground/nextjs/pages/survey.tsx
@@ -5,7 +5,8 @@ import { Survey } from 'posthog-js'
 export default function SurveyForm() {
     const posthog = usePostHog()
     const [surveys, setSurveys] = useState([] as unknown as Survey[])
-    const [selectedSurvey, setSelectedSurvey] = useState('0190bc7b-7096-0000-126d-1e5e7021a80e')
+    const [selectedSurvey, setSelectedSurvey] = useState('')
+
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedSurvey(event.target.value)
     }
@@ -13,24 +14,35 @@ export default function SurveyForm() {
     useEffect(() => {
         posthog.surveys.getSurveys((surveys: Survey[]) => {
             setSurveys(surveys)
+            if (surveys.length > 0) {
+                setSelectedSurvey(surveys[0].id)
+            }
         })
     }, [])
 
-    const arraySurveyItems = surveys.map((survey) => <option value={survey.id}> {survey.name}</option>)
+    const arraySurveyItems = surveys.map((survey) => (
+        <option key={survey.id} value={survey.id}>
+            {survey.name}
+        </option>
+    ))
 
     return (
         <>
             <div className="flex items-center gap-2 flex-wrap">
                 <select value={selectedSurvey} onChange={handleChange}>
+                    <option value="">Select a survey</option>
                     {arraySurveyItems}
                 </select>
-                <button onClick={() => posthog.renderSurvey(selectedSurvey, '#survey-container')}>
+                <button
+                    onClick={() => posthog.renderSurvey(selectedSurvey, '#survey-container')}
+                    disabled={!selectedSurvey}
+                >
                     Render Survey below
                 </button>
             </div>
             <div className="flex items-center gap-2 flex-wrap">
                 <div id="survey-container">
-                    <h1> hello world </h1>
+                    <h1>hello world</h1>
                 </div>
             </div>
         </>
