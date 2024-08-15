f030c70247c874e59ff0975977cb20502708790e
Robbie Coomber <robbie.coomber@gmail.com>
2024-08-15 17:15:24 +0100

    Remove duplicate lib version

--- src/posthog-core.ts
+++ src/posthog-core.ts
@@ -234,7 +234,6 @@ class DeprecatedWebPerformanceObserver {
 export class PostHog {
     __loaded: boolean
     config: PostHogConfig
-    lib_version: string
 
     rateLimiter: RateLimiter
     scrollManager: ScrollManager
@@ -287,7 +286,6 @@ export class PostHog {
         this.__request_queue = []
         this.__loaded = false
         this.analyticsDefaultEndpoint = '/e/'
-        this.lib_version = Config.LIB_VERSION
 
         this.featureFlags = new PostHogFeatureFlags(this)
         this.toolbar = new Toolbar(this)
