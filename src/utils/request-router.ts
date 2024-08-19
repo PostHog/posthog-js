be8cdb43800cb9430020be5da8be9fbc38757fbb
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-08-19 16:25:49 -0700

    cleaner implementation

--- src/utils/request-router.ts
+++ src/utils/request-router.ts
@@ -34,11 +34,18 @@ export class RequestRouter {
         return host
     }
     get uiHost(): string | undefined {
-        const host = this.instance.config.ui_host?.replace(/\/$/, '')
+        let host = this.instance.config.ui_host?.replace(/\/$/, '')
+
+        if (!host) {
+            // No ui_host set, get it from the api_host. But api_host differs
+            // from the actual UI host, so replace the ingestion subdomain with just posthog.com
+            host = this.apiHost.replace(`.${ingestionDomain}`, '.posthog.com')
+        }
 
         if (host === 'https://app.posthog.com') {
             return 'https://us.posthog.com'
         }
+
         return host
     }
 
@@ -62,7 +69,7 @@ export class RequestRouter {
         }
 
         if (target === 'ui') {
-            return (this.uiHost || this.apiHost.replace(`.${ingestionDomain}`, '.posthog.com')) + path
+            return this.uiHost + path
         }
 
         if (this.region === RequestRouterRegion.CUSTOM) {
