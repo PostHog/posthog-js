94ec707c9f6727511858bd9bc8ad92779d9f4592
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-08-20 17:44:25 +0200

    fix: capture a `$pageview` event on opting in

--- src/posthog-core.ts
+++ src/posthog-core.ts
@@ -260,6 +260,7 @@ export class PostHog {
     sessionRecording?: SessionRecording
     webPerformance = new DeprecatedWebPerformanceObserver()
 
+    _initialPageviewCaptured: boolean
     _triggered_notifs: any
     compression?: Compression
     __request_queue: QueuedRequestOptions[]
@@ -287,6 +288,7 @@ export class PostHog {
         this.__request_queue = []
         this.__loaded = false
         this.analyticsDefaultEndpoint = '/e/'
+        this._initialPageviewCaptured = false
 
         this.featureFlags = new PostHogFeatureFlags(this)
         this.toolbar = new Toolbar(this)
@@ -559,7 +561,8 @@ export class PostHog {
             // NOTE: We want to fire this on the next tick as the previous implementation had this side effect
             // and some clients may rely on it
             setTimeout(() => {
-                if (document) {
+                if (document && this.consent.isOptedIn() && !this._initialPageviewCaptured) {
+                    this._initialPageviewCaptured = true
                     this.capture('$pageview', { title: document.title }, { send_instantly: true })
                 }
             }, 1)
@@ -1983,12 +1986,15 @@ export class PostHog {
         this.consent.optInOut(true)
         this._sync_opt_out_with_persistence()
 
-        if (!isUndefined(options?.captureEventName) && !options?.captureEventName) {
-            // Don't capture if captureEventName is null or false
-            return
+        // Don't capture if captureEventName is null or false
+        if (isUndefined(options?.captureEventName) || options?.captureEventName) {
+            this.capture(options?.captureEventName ?? '$opt_in', options?.captureProperties, { send_instantly: true })
         }
 
-        this.capture(options?.captureEventName ?? '$opt_in', options?.captureProperties, { send_instantly: true })
+        if (this.config.capture_pageview && !this._initialPageviewCaptured && document) {
+            this._initialPageviewCaptured = true
+            this.capture('$pageview', { title: document.title }, { send_instantly: true })
+        }
     }
 
     /**
