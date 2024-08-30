38e47fe89b28eb2c9289bf3ff4e0ad100a986bb5
Paul D'Ambra <paul@posthog.com>
2024-08-30 12:13:39 +0100

    reimplement bug here

--- src/entrypoints/recorder.ts
+++ src/entrypoints/recorder.ts
@@ -513,7 +513,7 @@ function initFetchObserver(
                 }
 
                 after = win.performance.now()
-                res = await originalFetch(req)
+                res = await originalFetch(url, init)
                 before = win.performance.now()
 
                 const responseHeaders: Headers = {}
