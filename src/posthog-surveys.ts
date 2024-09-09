c76ea97cf3ae8fd707f76657bbb1489ea060d4ab
Paul D'Ambra <paul@posthog.com>
2024-09-09 17:13:46 +0100

    fix: warn on unavailable lazy load not throw (#1400)
    * fix: warn on unavailable lazy load not throw
    
    * and a default

--- src/posthog-surveys.ts
+++ src/posthog-surveys.ts
@@ -265,8 +265,9 @@ export class PostHogSurveys {
 
     // this method is lazily loaded onto the window to avoid loading preact and other dependencies if surveys is not enabled
     private _canActivateRepeatedly(survey: Survey) {
-        if (isNullish(assignableWindow.__PosthogExtensions__.canActivateRepeatedly)) {
+        if (isNullish(assignableWindow.__PosthogExtensions__?.canActivateRepeatedly)) {
             logger.warn(LOGGER_PREFIX, 'canActivateRepeatedly is not defined, must init before calling')
+            return false // TODO does it make sense to have a default here?
         }
         return assignableWindow.__PosthogExtensions__.canActivateRepeatedly(survey)
     }
