8d2909a5e67ce7500262f7d0050f0ef875303e54
Paul D'Ambra <paul@posthog.com>
2024-09-09 19:48:47 +0100

    fix: some clients need web vitals directly on window (#1406)
    * fix: some clients need web vitals directly on window
    
    * fix

--- src/entrypoints/web-vitals.ts
+++ src/entrypoints/web-vitals.ts
@@ -12,4 +12,11 @@ const postHogWebVitalsCallbacks = {
 assignableWindow.__PosthogExtensions__ = assignableWindow.__PosthogExtensions__ || {}
 assignableWindow.__PosthogExtensions__.postHogWebVitalsCallbacks = postHogWebVitalsCallbacks
 
+// we used to put posthogWebVitalsCallbacks on window, and now we put it on __PosthogExtensions__
+// but that means that old clients which lazily load this extension are looking in the wrong place
+// yuck,
+// so we also put it directly on the window
+// when 1.161.1 is the oldest version seen in production we can remove this
+assignableWindow.postHogWebVitalsCallbacks = postHogWebVitalsCallbacks
+
 export default postHogWebVitalsCallbacks
