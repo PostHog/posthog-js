79e065791027115dd555862d8af37272422ae415
David Newell <d.newell1@outlook.com>
2024-10-01 17:03:45 +0100

    fix: skip addition of `src` attribute for HLS videos (#1446)

--- playground/nextjs/pages/replay-examples/media.tsx
+++ playground/nextjs/pages/replay-examples/media.tsx
@@ -1,4 +1,22 @@
+import Hls from 'hls.js'
+import { useEffect, useRef } from 'react'
+
 export default function Media() {
+    const hlsVideoEl = useRef<HTMLVideoElement>(null)
+
+    useEffect(() => {
+        const videoEl = hlsVideoEl.current
+        if (videoEl) {
+            if (Hls.isSupported()) {
+                const hls = new Hls()
+                hls.loadSource('https://d2zihajmogu5jn.cloudfront.net/big-buck-bunny/master.m3u8')
+                hls.attachMedia(videoEl)
+            } else if (videoEl.canPlayType('application/vnd.apple.mpegurl')) {
+                videoEl.src = 'https://d2zihajmogu5jn.cloudfront.net/big-buck-bunny/master.m3u8'
+            }
+        }
+    }, [hlsVideoEl])
+
     return (
         <>
             <h1>Video</h1>
@@ -6,8 +24,18 @@ export default function Media() {
             <div style={{ margin: 10 }}>
                 <h3>Video</h3>
                 <video controls={true} style={{ width: 500 }}>
-                    <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+                    <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
                 </video>
+            </div>
+
+            <div style={{ margin: 10 }}>
+                <h3>HLS Video</h3>
+                <video
+                    ref={hlsVideoEl}
+                    controls={true}
+                    style={{ width: 500 }}
+                    hls-src="https://d2zihajmogu5jn.cloudfront.net/big-buck-bunny/master.m3u8"
+                />
             </div>
 
             <div style={{ margin: 10 }}>
