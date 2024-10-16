03e68118e3c9755dfc83f5f6173742b18fea2d37
Paul D'Ambra <paul@posthog.com>
2024-10-16 14:55:40 +0100

    doh

--- src/entrypoints/array.full.ts
+++ src/entrypoints/array.full.ts
@@ -1,5 +1,5 @@
 // for IE11 support we import this polyfill
-import './polyfills/array-from-polyfill.ts'
+import '../utils/array-polyfill'
 // Same as loader-globals.ts except includes all additional extension loaders
 import './all-external-dependencies'
 import './array'
--- src/entrypoints/array.ts
+++ src/entrypoints/array.ts
@@ -1,4 +1,4 @@
 // for IE11 support we import this polyfill
-import './polyfills/array-from-polyfill.ts'
+import '../utils/array-polyfill'
 import './external-scripts-loader'
 import './array.no-external'
