e1f244d69ac0d51a03985c8e4efea9d46998dd71
Paul D'Ambra <paul@posthog.com>
2024-10-16 14:52:23 +0100

    add array from polyfill

--- src/entrypoints/array.full.ts
+++ src/entrypoints/array.full.ts
@@ -1,3 +1,5 @@
+// for IE11 support we import this polyfill
+import './polyfills/array-from-polyfill.ts'
 // Same as loader-globals.ts except includes all additional extension loaders
 import './all-external-dependencies'
 import './array'
--- src/entrypoints/array.ts
+++ src/entrypoints/array.ts
@@ -1,2 +1,4 @@
+// for IE11 support we import this polyfill
+import './polyfills/array-from-polyfill.ts'
 import './external-scripts-loader'
 import './array.no-external'
