b70667f5b162d5b45c6bb4b58288c86036959302
Paul D'Ambra <paul@posthog.com>
2024-10-16 23:58:10 +0100

    ie11 fangling

--- src/entrypoints/array-ie11.ts
+++ src/entrypoints/array-ie11.ts
@@ -1,6 +1,11 @@
-// a straight copy of the array.ts entrypoint,
+// a straight copy of the array.full.ts entrypoint,
 // but will have different config when passed through rollup
 // to allow IE11 support
 
-import './external-scripts-loader'
+// it doesn't include recorder which doesn't support IE11,
+// and it doesn't include web-vitals which doesn't support IE11
+
+import './surveys'
+import './exception-autocapture'
+import './tracing-headers'
 import './array.no-external'
