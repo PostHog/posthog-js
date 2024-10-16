fd6439d137851f0ddbc8d8eb79f81e30716f007d
Paul D'Ambra <paul@posthog.com>
2024-10-16 19:02:08 +0100

    i too love ie11

--- src/entrypoints/array.full.ts
+++ src/entrypoints/array.full.ts
@@ -1,4 +1,4 @@
-import 'core-js/features/array/from'
+import 'core-js/actual/array/from'
 
 // Same as loader-globals.ts except includes all additional extension loaders
 import './all-external-dependencies'
