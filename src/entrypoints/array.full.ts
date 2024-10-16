3b2a40dcb4494baae42f9f2efbcf706b501e4f1e
Paul D'Ambra <paul@posthog.com>
2024-10-16 16:47:52 +0100

    what about a real polyfill

--- src/entrypoints/array.full.ts
+++ src/entrypoints/array.full.ts
@@ -1,5 +1,3 @@
-// for IE11 support we import this polyfill
-import '../utils/array-polyfill'
 // Same as loader-globals.ts except includes all additional extension loaders
 import './all-external-dependencies'
 import './array'
--- src/entrypoints/array.ts
+++ src/entrypoints/array.ts
@@ -1,4 +1,4 @@
-// for IE11 support we import this polyfill
-import '../utils/array-polyfill'
+import 'core-js/features/array/from'
+
 import './external-scripts-loader'
 import './array.no-external'
