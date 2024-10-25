a4e4bb28898e604f8f0b2a5135b4f7672ac9b159
Paul D'Ambra <paul@posthog.com>
2024-10-25 20:26:21 +0100

    chore: add recorder to es5 bundle

--- src/entrypoints/array.full.es5.ts
+++ src/entrypoints/array.full.es5.ts
@@ -2,10 +2,10 @@
 // but will have different config when passed through rollup
 // to allow es5/IE11 support
 
-// it doesn't include recorder which doesn't support IE11,
-// and it doesn't include web-vitals which doesn't support IE11
+// but it doesn't include web-vitals which doesn't support IE11
 
 import './surveys'
 import './exception-autocapture'
 import './tracing-headers'
+import './recorder'
 import './array.no-external'
