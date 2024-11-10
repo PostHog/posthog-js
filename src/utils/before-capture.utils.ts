070413dd12415222222a4471a63d3b53a6d06f41
Paul D'Ambra <paul@posthog.com>
2024-11-10 12:35:56 +0000

    Refactor

--- src/utils/before-capture.utils.ts
+++ src/utils/before-capture.utils.ts
@@ -11,8 +11,13 @@ function simpleHash(str: string) {
     return Math.abs(hash)
 }
 
+function sampleOnProperty(prop: string, percent: number): boolean {
+    return simpleHash(prop) % 100 < clampToRange(percent, 0, 100)
+}
+
 /**
- * An implementation of sampling that samples based on the distinct ID.
+ * Provides an implementation of sampling that samples based on the distinct ID.
+ * Using the provided percentage.
  * Can be used to create a beforeCapture fn for a PostHog instance.
  *
  * Causes roughly 50% of distinct ids to have events sent.
@@ -22,13 +27,13 @@ function simpleHash(str: string) {
  */
 export function sampleByDistinctId(percent: number): (c: CaptureResult) => CaptureResult | null {
     return (captureResult: CaptureResult): CaptureResult | null => {
-        const hash = simpleHash(captureResult.properties.distinct_id)
-        return hash % 100 < clampToRange(percent, 0, 100) ? captureResult : null
+        return sampleOnProperty(captureResult.properties.distinct_id, percent) ? captureResult : null
     }
 }
 
 /**
- * An implementation of sampling that samples based on the session ID.
+ * Provides an implementation of sampling that samples based on the session ID.
+ * Using the provided percentage.
  * Can be used to create a beforeCapture fn for a PostHog instance.
  *
  * Causes roughly 50% of sessions to have events sent.
@@ -38,13 +43,13 @@ export function sampleByDistinctId(percent: number): (c: CaptureResult) => Captu
  */
 export function sampleBySessionId(percent: number): (c: CaptureResult) => CaptureResult | null {
     return (captureResult: CaptureResult): CaptureResult | null => {
-        const hash = simpleHash(captureResult.properties.$session_id)
-        return hash % 100 < clampToRange(percent, 0, 100) ? captureResult : null
+        return sampleOnProperty(captureResult.properties.$session_id, percent) ? captureResult : null
     }
 }
 
 /**
- * An implementation of sampling that samples based on the event name.
+ * Provides an implementation of sampling that samples based on the event name.
+ * Using the provided percentage.
  * Can be used to create a beforeCapture fn for a PostHog instance.
  *
  * @param eventNames an array of event names to sample, sampling is applied across events not per event name
