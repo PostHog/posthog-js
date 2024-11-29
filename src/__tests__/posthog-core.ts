3ac6a9eaefa163356e67dcbbbed7f5dc8f82b332
David Newell <david@posthog.com>
2024-11-29 12:12:41 +0000

    fix tests

--- src/__tests__/posthog-core.ts
+++ src/__tests__/posthog-core.ts
@@ -437,6 +437,7 @@ describe('posthog core', () => {
                 $session_id: 'sessionId',
                 $is_identified: false,
                 $process_person_profile: false,
+                $recording_status: 'buffering',
             })
         })
 
