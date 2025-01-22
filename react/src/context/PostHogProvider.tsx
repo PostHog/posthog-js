906739182cf0b289d497697c1eaf56531c6fd782
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-01-22 15:22:49 -0300

    docs: Improve documentation/logs around PostHogProvider
    We're now logging more reasonable logs around our PostHogProvider to help customers identify problems with their setup. We're also typing that component much better to prevent people from getting it wrong at the type level.
    
    Inspired by #769, but not a solution

--- react/src/context/PostHogProvider.tsx
+++ react/src/context/PostHogProvider.tsx
@@ -4,41 +4,63 @@ import posthogJs, { PostHogConfig } from 'posthog-js'
 import React, { useMemo } from 'react'
 import { PostHog, PostHogContext } from './PostHogContext'
 
-export function PostHogProvider({
-    children,
-    client,
-    apiKey,
-    options,
-}: {
-    children?: React.ReactNode
-    client?: PostHog | undefined
-    apiKey?: string | undefined
-    options?: Partial<PostHogConfig> | undefined
-}) {
+type WithOptionalChildren<T> = T & { children?: React.ReactNode | undefined }
+
+/**
+ * Props for the PostHogProvider component.
+ * This is a discriminated union type that ensures mutually exclusive props:
+ *
+ * - If `client` is provided, `apiKey` and `options` must not be provided
+ * - If `apiKey` is provided, `client` must not be provided, and `options` is optional
+ */
+type PostHogProviderProps =
+    | { client: PostHog; apiKey?: never; options?: never }
+    | { apiKey: string; options?: Partial<PostHogConfig>; client?: never }
+
+/**
+ * PostHogProvider is a React context provider for PostHog analytics.
+ * It can be initialized in two mutually exclusive ways:
+ *
+ * 1. By providing an existing PostHog `client` instance
+ * 2. By providing an `apiKey` (and optionally `options`) to create a new client
+ *
+ * These initialization methods are mutually exclusive - you must use one or the other,
+ * but not both simultaneously.
+ */
+export function PostHogProvider({ children, client, apiKey, options }: WithOptionalChildren<PostHogProviderProps>) {
     const posthog = useMemo(() => {
-        if (client && apiKey) {
-            console.warn(
-                '[PostHog.js] You have provided both a client and an apiKey to PostHogProvider. The apiKey will be ignored in favour of the client.'
-            )
-        }
-
-        if (client && options) {
-            console.warn(
-                '[PostHog.js] You have provided both a client and options to PostHogProvider. The options will be ignored in favour of the client.'
-            )
-        }
-
         if (client) {
+            if (apiKey) {
+                console.warn(
+                    '[PostHog.js] You have provided both `client` and `apiKey` to `PostHogProvider`. `apiKey` will be ignored in favour of `client`.'
+                )
+            }
+
+            if (options) {
+                console.warn(
+                    '[PostHog.js] You have provided both `client` and `options` to `PostHogProvider`. `options` will be ignored in favour of `client`.'
+                )
+            }
+
+            if (client.__loaded) {
+                console.warn('[PostHog.js] `client` was already loaded elsewhere. This may cause issues.')
+            }
+
             return client
         }
 
         if (apiKey) {
             if (posthogJs.__loaded) {
-                console.warn('[PostHog.js] was already loaded elsewhere. This may cause issues.')
+                console.warn('[PostHog.js] `posthog` was already loaded elsewhere. This may cause issues.')
             }
+
             posthogJs.init(apiKey, options)
+            return posthogJs
         }
 
+        console.warn(
+            '[PostHog.js] No `apiKey` or `client` were provided to `PostHogProvider`. Using default global `window.posthog` instance. You must initialize it manually. This is not recommended behavior.'
+        )
         return posthogJs
     }, [client, apiKey])
 
--- react/src/context/__tests__/PostHogProvider.test.jsx
+++ react/src/context/__tests__/PostHogProvider.test.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render } from '@testing-library/react'
-import { PostHogProvider, PostHogContext } from '..'
+import { PostHogProvider } from '..'
 
 describe('PostHogProvider component', () => {
     given(
