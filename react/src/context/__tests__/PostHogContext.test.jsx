25b28ca9b7294d38fa6fe4e7297e0fdafa8a5118
Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2025-01-23 12:03:14 -0300

    docs: Improve documentation/logs around PostHogProvider (#1675)

--- react/src/context/__tests__/PostHogContext.test.jsx
+++ react/src/context/__tests__/PostHogContext.test.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { render } from '@testing-library/react'
-import { PostHogContext, PostHogProvider } from '../'
+import { PostHogProvider } from '..'
 
-describe('usePostHogContext hook', () => {
+describe('PostHogContext component', () => {
     given(
         'render',
         () => () =>
@@ -18,11 +18,17 @@ describe('usePostHogContext hook', () => {
         given.render()
     })
 
-    it("should not error if a client instance can't be found in the context", () => {
-        // used to make sure it doesn't throw an error when no client is found e.g. nextjs
-        given('posthog', () => undefined)
-        console.error = jest.fn()
+    it("should not throw error if a client instance can't be found in the context", () => {
+        given('posthog', () => undefined) // it might not exist in SSR for example
 
-        expect(() => given.render())
+        // eslint-disable-next-line no-console
+        console.warn = jest.fn()
+
+        expect(() => given.render()).not.toThrow()
+
+        // eslint-disable-next-line no-console
+        expect(console.warn).toHaveBeenCalledWith(
+            '[PostHog.js] No `apiKey` or `client` were provided to `PostHogProvider`. Using default global `window.posthog` instance. You must initialize it manually. This is not recommended behavior.'
+        )
     })
 })
