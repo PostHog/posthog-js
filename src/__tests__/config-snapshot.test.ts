60d70c25c5f24f4f79442e9a44e42067aab61a9f
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-01-30 11:22:00 -0300

    test: Simplify `Regexp` rule
    We don't need to look inside `Regexp`, it's fine to render it as is

--- src/__tests__/config-snapshot.test.ts
+++ src/__tests__/config-snapshot.test.ts
@@ -1,6 +1,7 @@
 import ts from 'typescript'
 import path from 'path'
 
+type ProcessedType = string | Record<string, string | string[] | Record<string, any> | any[]> | ProcessedType[]
 function extractTypeInfo(filePath: string, typeName: string): string {
     const program = ts.createProgram([filePath], { noImgplicitAny: true, strictNullChecks: true })
     const checker = program.getTypeChecker()
@@ -14,22 +15,31 @@ function extractTypeInfo(filePath: string, typeName: string): string {
         return checker.typeToString(type)
     }
 
-    function processType(type: ts.Type): any {
+    function processType(type: ts.Type): ProcessedType {
+        // Early detect RegExp type and return its string representation
+        // No need to recursively resolve it
+        if (type.symbol?.name === 'RegExp') {
+            return getTypeString(type)
+        }
+
         if (type.isUnion() || type.isIntersection()) {
             return type.types.map(processType)
-        } else if (type.isClassOrInterface()) {
-            const result: Record<string, any> = {}
+        }
+
+        if (type.isClassOrInterface()) {
+            const result: Record<string, ProcessedType> = {}
             type.getProperties().forEach((symbol) => {
                 const propType = checker.getTypeOfSymbol(symbol)
                 result[symbol.getName()] = processType(propType)
             })
+
             return result
         }
 
         return getTypeString(type)
     }
 
-    let result: Record<string, any> = {}
+    let result: ProcessedType = {}
 
     ts.forEachChild(sourceFile, (node) => {
         if (ts.isInterfaceDeclaration(node) && node.name.text === typeName) {
@@ -41,8 +51,12 @@ function extractTypeInfo(filePath: string, typeName: string): string {
     return JSON.stringify(result, null, 2)
 }
 
+// This guarantees that the config types are stable and won't change
+// or that, at least, we won't ever remove any options from the config
+// and/or change the types of existing options.
 describe('config snapshot', () => {
     it('for PostHogConfig', () => {
-        expect(extractTypeInfo(path.resolve(__dirname, '../types.ts'), 'PostHogConfig')).toMatchSnapshot()
+        const typeInfo = extractTypeInfo(path.resolve(__dirname, '../types.ts'), 'PostHogConfig')
+        expect(typeInfo).toMatchSnapshot()
     })
 })
