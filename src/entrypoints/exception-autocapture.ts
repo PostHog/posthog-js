80ff82865be4cfb506ea8741afaf8c68a8554794
David Newell <david@posthog.com>
2025-02-10 12:45:44 +0000

    fix: capture when argument is not a string

--- src/entrypoints/exception-autocapture.ts
+++ src/entrypoints/exception-autocapture.ts
@@ -12,10 +12,10 @@ const wrapOnError = (captureFn: (props: Properties) => void) => {
     }
     const originalOnError = win.onerror
 
-    win.onerror = function (...args: ErrorEventArgs): boolean {
-        const errorProperties = errorToProperties(args)
+    win.onerror = function (args: ErrorEventArgs): boolean {
+        const errorProperties = errorToProperties({ event: args[0], error: args[4] })
         captureFn(errorProperties)
-        return originalOnError?.(...args) ?? false
+        return originalOnError?.(args) ?? false
     }
     win.onerror.__POSTHOG_INSTRUMENTED__ = true
 
