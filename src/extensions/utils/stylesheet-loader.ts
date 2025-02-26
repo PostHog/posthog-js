060edce699245136682f8f861bd6a4e3babead23
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2025-02-26 17:28:16 +0100

    fix: nonce missing style script (#1765)

--- src/extensions/utils/stylesheet-loader.ts
+++ src/extensions/utils/stylesheet-loader.ts
@@ -1,15 +1,14 @@
 import { PostHog } from '../../posthog-core'
-import { document } from '../../utils/globals'
 import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('[Stylesheet Loader]')
 
-export const prepareStylesheet = (innerText: string, posthog?: PostHog) => {
+export const prepareStylesheet = (document: Document, innerText: string, posthog?: PostHog) => {
     // Forcing the existence of `document` requires this function to be called in a browser environment
-    let stylesheet: HTMLStyleElement | null = document!.createElement('style')
+    let stylesheet: HTMLStyleElement | null = document.createElement('style')
     stylesheet.innerText = innerText
 
-    if (posthog?.config.prepare_external_dependency_stylesheet) {
+    if (posthog?.config?.prepare_external_dependency_stylesheet) {
         stylesheet = posthog.config.prepare_external_dependency_stylesheet(stylesheet)
     }
 
