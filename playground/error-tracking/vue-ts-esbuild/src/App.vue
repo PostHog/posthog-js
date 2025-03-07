e245663ebf2f8dd52a1fdfb4bbe4f1af0881c832
Hugues Pouillot <hpouillot@gmail.com>
2025-03-07 16:14:04 +0000

    add vite ts playground (#1797)

---
+++ playground/error-tracking/vue-ts-esbuild/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ErrorButton from './components/ErrorButton.vue'
+</script>
+
+<template>
+    <ErrorButton msg="vite + vue" />
+</template>
---
+++ playground/error-tracking/vue-ts-esbuild/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        sourcemap: true,
+        minify: 'esbuild',
+    },
+})
