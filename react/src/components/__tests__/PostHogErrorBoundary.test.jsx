e27ad000e5157876d8936c4b01f1486e6e26c6c3
Hugues Pouillot <hpouillot@gmail.com>
2025-04-09 19:41:11 +0200

    test rendering without errors

--- react/src/components/__tests__/PostHogErrorBoundary.test.jsx
+++ react/src/components/__tests__/PostHogErrorBoundary.test.jsx
@@ -20,7 +20,7 @@ describe('PostHogErrorBoundary component', () => {
     })
 
     given(
-        'render',
+        'render_with_error',
         () =>
             ({ message = 'Error', fallback = <div></div>, additionalProperties }) =>
                 render(
@@ -32,35 +32,46 @@ describe('PostHogErrorBoundary component', () => {
                 )
     )
 
+    given(
+        'render_without_error',
+        () => () =>
+            render(
+                <PostHogProvider client={given.posthog}>
+                    <PostHogErrorBoundary>
+                        <div>Amazing content</div>
+                    </PostHogErrorBoundary>
+                </PostHogProvider>
+            )
+    )
+
     given('posthog', () => ({
         captureException: jest.fn(),
     }))
 
     it('should call captureException with error message', () => {
-        const { container } = given.render({ message: 'Test error', fallback: <div></div> })
+        const { container } = given.render_with_error({ message: 'Test error', fallback: <div></div> })
         expect(given.posthog.captureException).toHaveBeenCalledWith(new Error('Test error'), undefined)
         expect(container.innerHTML).toBe('<div></div>')
         expect(console.error).toHaveBeenCalledTimes(2)
     })
 
     it('should warn user when fallback is not null', () => {
-        const { container } = given.render({ fallback: null })
+        const { container } = given.render_with_error({ fallback: null })
         expect(given.posthog.captureException).toHaveBeenCalledWith(new Error('Error'), undefined)
         expect(container.innerHTML).toBe('')
         expect(console.warn).toHaveBeenCalledWith(__POSTHOG_ERROR_WARNING_MESSAGES.INVALID_FALLBACK)
     })
 
     it('should warn user when fallback is a string', () => {
-        const { container } = given.render({ fallback: 'hello' })
+        const { container } = given.render_with_error({ fallback: 'hello' })
         expect(given.posthog.captureException).toHaveBeenCalledWith(new Error('Error'), undefined)
         expect(container.innerHTML).toBe('')
         expect(console.warn).toHaveBeenCalledWith(__POSTHOG_ERROR_WARNING_MESSAGES.INVALID_FALLBACK)
     })
 
-    it('should add additional properties to event', () => {
-        const props = { team_id: '1234' }
-        given.render({ message: 'Kaboom', additionalProperties: props })
-        expect(given.posthog.captureException).toHaveBeenCalledWith(new Error('Kaboom'), props)
+    it('should render children without errors', () => {
+        const { container } = given.render_without_error()
+        expect(container.innerHTML).toBe('<div>Amazing content</div>')
     })
 })
 
