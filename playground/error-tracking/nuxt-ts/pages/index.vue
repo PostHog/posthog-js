7488962ac0cb9415bd3ccfa8a355c71e69b7cebe
Hugues Pouillot <hpouillot@gmail.com>
2025-04-24 17:01:05 +0200

    simplify example

--- playground/error-tracking/nuxt-ts/pages/index.vue
+++ playground/error-tracking/nuxt-ts/pages/index.vue
@@ -1,4 +1,17 @@
+<script setup lang="ts">
+const { $posthog } = useNuxtApp()
+
+function captureException() {
+    // manually capture exception
+    $posthog().captureException(new Error('Nuxt manual error'))
+}
+
+function throwException() {
+    // exception will be captured by vue:error hook defined in posthog.client.js
+    throw new Error('Nuxt manual error')
+}
+</script>
 <template>
-    <ErrorButton/>
-    <NuxtLink to="/error">Go to error page</NuxtLink>
+    <button @click="captureException()">Capture Exception</button>
+    <button @click="throwException()">Throw Exception</button>
 </template>
