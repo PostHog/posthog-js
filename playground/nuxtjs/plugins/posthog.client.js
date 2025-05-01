dbc784bc4c0acee03a6f958808b1f8bac4c2edb0
Lucas Ricoy <2034367+lricoy@users.noreply.github.com>
2025-05-01 15:59:20 +0100

    Update playground/nuxtjs/plugins/posthog.client.js
    Co-authored-by: Robbie <robbie.coomber@gmail.com>

--- playground/nuxtjs/plugins/posthog.client.js
+++ playground/nuxtjs/plugins/posthog.client.js
@@ -7,7 +7,6 @@ export default defineNuxtPlugin(() => {
     const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
         api_host: runtimeConfig.public.posthogHost,
         capture_pageview: 'history_change',
-        capture_pageleave: true, // automatically capture a pageleave event when the user leaves the site or closes the tab
         loaded: (posthog) => {
             if (import.meta.env.MODE === 'development') posthog.debug()
         },
