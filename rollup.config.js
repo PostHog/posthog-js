b7d21c4fa9b71c2424d61e394bea7b3c22e43cb4
Lucas Faria <lucasheriques@gmail.com>
2025-05-10 03:08:51 -0300

    Revert "see if bundle size reduces"
    This reverts commit 49ca67a11629211a6c30def0eff8613403b10530.

--- rollup.config.js
+++ rollup.config.js
@@ -28,20 +28,7 @@ const plugins = (es5) => [
             postcssImport(),
             postcssNesting(),
             cssnano({
-                preset: [
-                    'default',
-                    {
-                        discardComments: { removeAll: true },
-                        normalizeWhitespace: true,
-                        minifyFontValues: { removeQuotes: false },
-                        minifyGradients: true,
-                        convertValues: true,
-                        reduceInitial: true,
-                        mergeRules: true,
-                        discardUnused: true,
-                        // ...add more aggressive options as needed
-                    },
-                ],
+                preset: ['default', { discardComments: { removeAll: true } }],
             }),
         ],
         minimize: true,
