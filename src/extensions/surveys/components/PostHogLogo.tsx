a476f92f04eae7e910004b7b1b4c66537d7eb673
Lucas Faria <lucasheriques@gmail.com>
2025-05-10 16:40:55 -0300

    use opacity transitions for emoji fill

--- src/extensions/surveys/components/PostHogLogo.tsx
+++ src/extensions/surveys/components/PostHogLogo.tsx
@@ -2,7 +2,7 @@ import { IconPosthogLogo } from '../icons'
 
 export function PostHogLogo() {
     return (
-        <a href="https://posthog.com" target="_blank" rel="noopener" className="footer-branding">
+        <a href="https://posthog.com/surveys" target="_blank" rel="noopener" className="footer-branding">
             Survey by {IconPosthogLogo}
         </a>
     )
