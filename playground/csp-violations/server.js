f277b9fcc12b275d1595fd8a5e568d341d8e0db6
Robbie Coomber <robbie.coomber@gmail.com>
2025-05-14 16:25:04 +0100

    Add some extra things

--- playground/csp-violations/server.js
+++ playground/csp-violations/server.js
@@ -1,19 +1,49 @@
 const express = require('express')
 const path = require('path')
+const { v7: uuidv7 } = require('uuid')
 const app = express()
 const PORT = 8080
 
 // UPDATE YOUR TOKEN!!!
-const CSP_REPORT_URI = 'http://localhost:8010/csp?token=phc_Pv7thRPMKG4x2lOBamiZHgo5kDW7vuGJeWqp978dlFg'
+const POSTHOG_TOKEN = 'phc_tQttuNsuNT6bHLQTFvvxIHjXXcOKWIk7NySsAjw42EF'
+const POSTHOG_SNIPPET = `<script>
+    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init bs ws ge fs capture De calculateEventProperties $s register register_once register_for_session unregister unregister_for_session Is getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty xs Ss createPersonProfile Es gs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing ys debug ks getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init("${POSTHOG_TOKEN}", {
+        api_host: 'http://localhost:8010',
+        person_profiles: 'identified_only',
+    })
+</script>`
+
+const CSP_REPORT_URI = `http://localhost:8010/csp?token=${POSTHOG_TOKEN}&pid=${uuidv7()}`
+const USE_REPORT_TO = CSP_REPORT_URI.startsWith('https://')
+
+const CSP_RULES = {
+    'default-src': "'self'",
+    'script-src': "'self' https://*.posthog.com http://localhost:8010 'unsafe-inline'",
+    'connect-src': "'self' https://*.posthog.com http://localhost:8010",
+    'img-src': "'self' data:",
+    'style-src': "'self'",
+    'report-uri': CSP_REPORT_URI,
+    ...(USE_REPORT_TO
+        ? {
+              'report-to': 'csp-endpoint', // easier to debug with report-uri
+          }
+        : {}),
+}
+
+const CSP_HEADER = Object.entries(CSP_RULES)
+    .map(([key, value]) => `${key} ${value}`)
+    .join('; ')
+const REPORTING_HEADER = `csp-endpoint="${CSP_REPORT_URI}"`
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
     // Set CSP as headers, we can probably make this a bit more configurable for the playground
-    res.setHeader(
-        'Content-Security-Policy',
-        `default-src 'self'; script-src 'self' https://*.posthog.com; connect-src 'self' https://*.posthog.com; img-src 'self' data:; style-src 'self'; report-uri ${CSP_REPORT_URI}`
-    )
+    res.setHeader('Content-Security-Policy', CSP_HEADER)
+    if (USE_REPORT_TO) {
+        res.setHeader('Reporting-Endpoints', REPORTING_HEADER)
+    }
     next()
 })
 
@@ -66,6 +96,7 @@ app.get('/', (req, res) => {
           overflow-x: auto;
         }
       </style>
+      ${POSTHOG_SNIPPET}
     </head>
     <body>
       <h1>CSP Violation Playground</h1>
@@ -86,12 +117,7 @@ app.get('/', (req, res) => {
         <p>This playground is configured to send CSP violation reports to:</p>
         <pre>${CSP_REPORT_URI}</pre>
         <p>Current CSP policy:</p>
-        <pre>default-src 'self';
-script-src 'self' https://*.posthog.com;
-connect-src 'self' https://*.posthog.com;
-img-src 'self' data:;
-style-src 'self';
-report-uri ${CSP_REPORT_URI};</pre>
+        <pre>${JSON.stringify(CSP_RULES, null, 2)}</pre>
       </div>
     </body>
     </html>
@@ -147,6 +173,7 @@ app.get('/external-script', (req, res) => {
       </style>
       <!-- This external script violates CSP -->
       <script src="https://example.com/script.js"></script>
+      ${POSTHOG_SNIPPET}
     </head>
     <body>
       <h1>External Script Violation</h1>
@@ -174,6 +201,7 @@ app.get('/external-img', (req, res) => {
         .card { border: 1px solid #ddd; padding: 20px; border-radius: 8px; }
         a { color: #4f46e5; }
       </style>
+      ${POSTHOG_SNIPPET}
     </head>
     <body>
       <h1>External Image Violation</h1>
@@ -205,6 +233,7 @@ app.get('/external-style', (req, res) => {
         .card { border: 1px solid #ddd; padding: 20px; border-radius: 8px; }
         a { color: #4f46e5; }
       </style>
+      ${POSTHOG_SNIPPET}
     </head>
     <body>
       <h1>External Style Violation</h1>
@@ -232,6 +261,7 @@ app.get('/xhr-violation', (req, res) => {
         .card { border: 1px solid #ddd; padding: 20px; border-radius: 8px; }
         a { color: #4f46e5; }
       </style>
+      ${POSTHOG_SNIPPET}
     </head>
     <body>
       <h1>XHR Violation</h1>
