20a1c9a9672c87ab022a582661a8b332f1175ac9
Hugues Pouillot <hpouillot@gmail.com>
2025-06-06 18:31:30 +0200

    split linting config

--- packages/browser/.eslintrc.js
+++ packages/browser/.eslintrc.js
@@ -1,85 +1,24 @@
 /*eslint-env node */
 
-// https://eslint.org/docs/v8.x/use/configure/configuration-files
-
-const rules = {
-    'prettier/prettier': 'error',
-    'prefer-spread': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-this-alias': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-unused-expressions': 'off',
-    'no-prototype-builtins': 'off',
-    'no-empty': 'off',
-    'no-console': 'error',
-    'no-only-tests/no-only-tests': 'error',
-    'posthog-js/no-external-replay-imports': 'error',
-    '@typescript-eslint/naming-convention': [
-        'error',
-        {
-            selector: ['memberLike'],
-            modifiers: ['private'],
-            format: null,
-            leadingUnderscore: 'require',
-        },
-    ],
-}
-
-const extend = [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:compat/recommended',
-    'plugin:posthog-js/all',
-]
-
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es6: true,
-        'jest/globals': true,
-    },
-    globals: {
-        given: 'readonly',
-        global: 'readonly',
-        Buffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
         project: true,
     },
-    plugins: [
-        'prettier',
-        '@typescript-eslint',
-        'eslint-plugin-react',
-        'eslint-plugin-react-hooks',
-        'jest',
-        'no-only-tests',
-    ],
-    extends: extend,
-    rules,
-    settings: {
-        react: {
-            version: '17.0',
-        },
-    },
     overrides: [
         {
-            files: 'packages/browser/src/**/*',
+            files: './src/**/*',
             rules: {
                 'no-restricted-globals': ['error', 'document', 'window'],
             },
         },
         {
-            files: 'packages/browser/src/__tests__/**/*',
+            files: './src/__tests__/**/*',
             // the same set of config as in the root
             // but excluding the 'plugin:compat/recommended' rule
             // we don't mind using the latest features in our tests
-            extends: extend.filter((s) => s !== 'plugin:compat/recommended'),
+            // extends: extend.filter((s) => s !== 'plugin:compat/recommended'),
             rules: {
                 'no-console': 'off',
                 'no-restricted-globals': 'off',
@@ -87,47 +26,20 @@ module.exports = {
             },
         },
         {
-            files: ['**/*.js'],
-            parserOptions: {
-                project: null, // <- prevents the TS parser from trying to parse it with type info
-            },
-            rules: {
-                '@typescript-eslint/naming-convention': 'off',
-            },
-        },
-        {
-            files: 'packages/browser/react/src/**/',
-            extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
-        },
-        {
-            files: 'eslint-rules/**/*',
-            extends: ['eslint:recommended', 'prettier'],
-            rules: {
-                'prettier/prettier': 'error',
-                '@typescript-eslint/no-var-requires': 'off',
-                '@typescript-eslint/no-require-imports': 'off',
-                'posthog-js/no-direct-null-check': 'off',
-                'posthog-js/no-direct-boolean-check': 'off',
-            },
-            env: {
-                node: true,
-            },
-        },
-        {
-            files: 'packages/browser/playground/cypress/**/*',
+            files: './playground/cypress/**/*',
             globals: {
                 cy: true,
                 Cypress: true,
             },
         },
         {
-            files: 'packages/browser/testcafe/**/*',
+            files: './testcafe/**/*',
             globals: {
                 __dirname: true,
             },
         },
         {
-            files: ['packages/browser/playwright/**/*.ts'],
+            files: './playwright/**/*.ts',
             rules: {
                 'posthog-js/no-direct-array-check': 'off',
                 'posthog-js/no-direct-undefined-check': 'off',
@@ -139,13 +51,7 @@ module.exports = {
             },
         },
         {
-            files: ['packages/browser/react/**/*.ts'],
-            rules: {
-                'posthog-js/no-direct-null-check': 'off',
-            },
-        },
-        {
-            files: ['packages/browser/playground/**/*'],
+            files: './playground/**/*',
             rules: {
                 'no-console': 'off',
                 '@typescript-eslint/no-require-imports': 'off',
@@ -159,6 +65,5 @@ module.exports = {
             },
         },
     ],
-    root: true,
-    ignorePatterns: ['packages/browser/playground/error-tracking/**/*'],
+    ignorePatterns: ['./playground/error-tracking/**/*'],
 }
