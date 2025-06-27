de00982cb66cbed1c4fe806ac970fabd7965610b
Hugues Pouillot <hpouillot@gmail.com>
2025-06-27 16:01:03 +0200

    chore(migration): update node package (#2052)
    autofix linting issue
    
    autofix linting issue
    
    update browser lint:fix script
    
    move eslint-rules
    
    add package metadata
    
    update module, resolution, lib
    
    add build script
    
    create posthog-core entrypoints
    
    remove unused dev dep
    
    update main entrypoint
    
    update react-native package
    
    revert target to es2018

--- packages/react-native/src/surveys/components/QuestionTypes.tsx
+++ packages/react-native/src/surveys/components/QuestionTypes.tsx
@@ -17,7 +17,7 @@ import {
   LinkSurveyQuestion,
   RatingSurveyQuestion,
   MultipleSurveyQuestion,
-} from '../../../../posthog-core/src'
+} from 'posthog-core'
 import { BottomSection } from './BottomSection'
 import { QuestionHeader } from './QuestionHeader'
 
