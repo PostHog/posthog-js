a03636d73f10e4e79534599186c97d4d4ce8b0a7
Hugues Pouillot <hpouillot@gmail.com>
2025-06-27 16:01:03 +0200

    chore(migration): update node package (#2052)
    add rollup-utils tooling package
    
    update dependencies
    
    update ai test and build config
    
    update prepublish script

---
+++ packages/ai/rollup.config.ts
@@ -0,0 +1,75 @@
+import { external, babel, resolve, commonjs, json, dts, plugins } from '@posthog-tooling/rollup-utils'
+import packageJson from './package.json'
+
+const configs = []
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const externalDeps = external(packageJson)
+
+configs.push({
+  input: `./src/index.ts`,
+  output: [
+    {
+      file: packageJson.main,
+      sourcemap: true,
+      exports: 'named',
+      format: `cjs`,
+    },
+    {
+      file: packageJson.module,
+      sourcemap: true,
+      format: `es`,
+    },
+  ],
+  external: externalDeps,
+  plugins: plugins(extensions),
+})
+
+configs.push({
+  input: `./src/index.ts`,
+  output: [{ file: `./lib/index.d.ts`, format: 'es' }],
+  external: externalDeps,
+  plugins: [resolve({ extensions }), dts()],
+})
+
+// Add submodule builds for posthog-ai
+const providers = ['anthropic', 'openai', 'vercel', 'langchain']
+
+providers.forEach((provider) => {
+  configs.push({
+    input: `./src/${provider}/index.ts`,
+    output: [
+      {
+        file: `./lib/${provider}/index.cjs`,
+        sourcemap: true,
+        exports: 'named',
+        format: 'cjs',
+      },
+      {
+        file: `./lib/${provider}/index.mjs`,
+        sourcemap: true,
+        format: 'es',
+      },
+    ],
+    external: externalDeps,
+    plugins: [
+      resolve({ extensions }),
+      commonjs(),
+      json(),
+      babel({
+        extensions,
+        babelHelpers: 'bundled',
+        include: ['./src/**/*.{js,jsx,ts,tsx}'],
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+      }),
+    ],
+  })
+
+  configs.push({
+    input: `./src/${provider}/index.ts`,
+    output: [{ file: `./lib/${provider}/index.d.ts`, format: 'es' }],
+    external: externalDeps,
+    plugins: [resolve({ extensions }), dts()],
+  })
+})
+
+export default configs
