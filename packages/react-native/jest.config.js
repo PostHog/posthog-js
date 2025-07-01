847fc437097b425430b8102f22bf138643e37726
Hugues Pouillot <hpouillot@gmail.com>
2025-07-01 14:13:07 +0200

    fix react native tests

--- packages/react-native/jest.config.js
+++ packages/react-native/jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   preset: 'jest-expo',
   roots: ['<rootDir>'],
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
   moduleFileExtensions: ['ts', 'js', 'json', 'node', 'tsx'],
   collectCoverage: true,
   clearMocks: true,
