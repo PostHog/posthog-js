4043a62bc73258bf348603942e5988429ccf02c7
Hugues Pouillot <hpouillot@gmail.com>
2025-07-01 14:59:33 +0200

    sync examples folder

--- examples/example_rn/android/app/src/main/java/com/example_rn/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example_rn
-
-import com.reactnativenavigation.NavigationActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
-
-class MainActivity : NavigationActivity()
