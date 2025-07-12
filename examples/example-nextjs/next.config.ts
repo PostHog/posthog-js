9e4d8c7053d250527778de5094e61c2d36c26a70
Hugues Pouillot <hpouillot@gmail.com>
2025-07-12 12:34:28 +0200

    update nextjs example

--- examples/example-nextjs/next.config.ts
+++ examples/example-nextjs/next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next'
 import { withPostHogConfig } from '@posthog/nextjs-config'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
 }
 
 export default withPostHogConfig(nextConfig, {
-  authToken: process.env.POSTHOG_PRIVATE_API_KEY!,
-  envId: '1',
-  host: 'http://localhost:8010',
+    personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY!,
+    envId: '1',
+    host: 'http://localhost:8010',
 })
--- examples/example-nextjs/src/app/posthog.tsx
+++ examples/example-nextjs/src/app/posthog.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import { PostHogConfig } from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+import { PostHogProvider } from '@posthog/react'
 
 const posthogConfig: Partial<PostHogConfig> = {
-  api_host: 'http://localhost:8010',
-  debug: process.env.NODE_ENV === 'development',
+    api_host: 'http://localhost:8010',
+    debug: process.env.NODE_ENV === 'development',
 }
 
 export default function PHProvider({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <PostHogProvider apiKey={process.env.NEXT_PUBLIC_POSTHOG_PROJECT_KEY!} options={posthogConfig}>
-      {children}
-    </PostHogProvider>
-  )
+    return (
+        <PostHogProvider apiKey={process.env.NEXT_PUBLIC_POSTHOG_PROJECT_KEY!} options={posthogConfig}>
+            {children}
+        </PostHogProvider>
+    )
 }
