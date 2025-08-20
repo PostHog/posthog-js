f13aebe09c853886d2609307d96da59e82428aa2
Hugues Pouillot <hpouillot@gmail.com>
2025-08-20 15:07:27 +0200

    update example-nextjs

--- examples/example-nextjs/next.config.ts
+++ examples/example-nextjs/next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next'
 import { withPostHogConfig } from '@posthog/nextjs-config'
+import { version } from './package.json'
 
 const nextConfig: NextConfig = {
     /* config options here */
@@ -9,4 +10,9 @@ export default withPostHogConfig(nextConfig, {
     personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY!,
     envId: process.env.POSTHOG_API_PROJECT!,
     host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST!,
+    verbose: true,
+    sourcemaps: {
+        project: 'example-nextjs',
+        version,
+    },
 })
