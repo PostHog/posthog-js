808894eab5406b67470f7b6c55c958be7c8dc0a6
Hugues Pouillot <hpouillot@gmail.com>
2025-08-20 15:10:05 +0200

    add turbopack support

--- packages/nextjs-config/src/config.ts
+++ packages/nextjs-config/src/config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next'
 import { SourcemapWebpackPlugin } from './webpack-plugin'
+import { hasCompilerHook, processSourceMaps } from 'utils'
 
 type NextFuncConfig = (phase: string, { defaultConfig }: { defaultConfig: NextConfig }) => NextConfig
 type NextAsyncConfig = (phase: string, { defaultConfig }: { defaultConfig: NextConfig }) => Promise<NextConfig>
@@ -33,31 +34,27 @@ export type PostHogNextConfigComplete = {
 
 export function withPostHogConfig(userNextConfig: UserProvidedConfig, posthogConfig: PostHogNextConfig): NextConfig {
   const posthogNextConfigComplete = resolvePostHogConfig(posthogConfig)
+  const sourceMapEnabled = posthogNextConfigComplete.sourcemaps.enabled
+  const isCompilerHookSupported = hasCompilerHook()
   return async (phase: string, { defaultConfig }: { defaultConfig: NextConfig }) => {
     const {
       webpack: userWebPackConfig,
+      compiler: userCompilerConfig,
       distDir,
       ...userConfig
     } = await resolveUserConfig(userNextConfig, phase, defaultConfig)
-    const defaultWebpackConfig = userWebPackConfig || ((config: any) => config)
-    const sourceMapEnabled = posthogNextConfigComplete.sourcemaps.enabled
     return {
       ...userConfig,
       distDir,
       productionBrowserSourceMaps: sourceMapEnabled,
-      webpack: (config: any, options: any) => {
-        const webpackConfig = defaultWebpackConfig(config, options)
-        if (webpackConfig && options.isServer && sourceMapEnabled) {
-          webpackConfig.devtool = 'source-map'
-        }
-        if (sourceMapEnabled) {
-          webpackConfig.plugins = webpackConfig.plugins || []
-          webpackConfig.plugins.push(
-            new SourcemapWebpackPlugin(posthogNextConfigComplete, options.isServer, options.nextRuntime, distDir)
-          )
-        }
-        return webpackConfig
-      },
+      webpack: withWebpackConfig(
+        userWebPackConfig,
+        sourceMapEnabled,
+        !isCompilerHookSupported,
+        posthogNextConfigComplete,
+        distDir
+      ),
+      compiler: withCompilerConfig(userCompilerConfig, sourceMapEnabled, posthogNextConfigComplete),
     }
   }
 }
@@ -95,4 +92,46 @@ function resolvePostHogConfig(posthogProvidedConfig: PostHogNextConfig): PostHog
       deleteAfterUpload: sourcemaps.deleteAfterUpload ?? true,
     },
   }
+}
+
+function withWebpackConfig(
+  userWebpackConfig: NextConfig['webpack'],
+  sourceMapEnabled: boolean,
+  injectPlugin: boolean,
+  posthogConfig: PostHogNextConfigComplete,
+  distDir: string | undefined
+) {
+  const defaultWebpackConfig = userWebpackConfig || ((config: any) => config)
+  return (config: any, options: any) => {
+    const webpackConfig = defaultWebpackConfig(config, options)
+    if (webpackConfig && options.isServer && sourceMapEnabled) {
+      webpackConfig.devtool = 'source-map'
+    }
+    if (sourceMapEnabled && injectPlugin) {
+      webpackConfig.plugins = webpackConfig.plugins || []
+      webpackConfig.plugins.push(
+        new SourcemapWebpackPlugin(posthogConfig, options.isServer, options.nextRuntime, distDir)
+      )
+    }
+    return webpackConfig
+  }
+}
+
+function withCompilerConfig(
+  userCompilerConfig: NextConfig['compiler'],
+  sourceMapEnabled: boolean,
+  posthogConfig: PostHogNextConfigComplete
+): NextConfig['compiler'] {
+  const newConfig = userCompilerConfig || {}
+  if (sourceMapEnabled && hasCompilerHook()) {
+    const userCompilerHook = userCompilerConfig?.runAfterProductionCompile
+    newConfig.runAfterProductionCompile = async (config: { distDir: string; projectDir: string }) => {
+      await userCompilerHook?.(config)
+      if (sourceMapEnabled) {
+        posthogConfig.verbose && console.debug('Processing source maps from compilation hook...')
+        await processSourceMaps(posthogConfig, config.distDir, posthogConfig.sourcemaps.deleteAfterUpload)
+      }
+    }
+  }
+  return newConfig
 }
--- packages/nextjs-config/src/webpack-plugin.ts
+++ packages/nextjs-config/src/webpack-plugin.ts
@@ -1,6 +1,6 @@
 import { PostHogNextConfigComplete } from './config'
 import path from 'path'
-import { callPosthogCli } from './utils'
+import { processSourceMaps } from './utils'
 
 type NextRuntime = 'edge' | 'nodejs' | undefined
 
@@ -37,8 +37,8 @@ export class SourcemapWebpackPlugin {
     const onDone = async (_: any, callback: any): Promise<void> => {
       callback = callback || (() => {})
       try {
-        await this.runInject()
-        await this.runUpload()
+        this.posthogOptions.verbose && console.log('Processing source maps from webpack plugin...')
+        await processSourceMaps(this.posthogOptions, this.directory, this.isServer)
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : error
         return console.error('Error running PostHog sourcemap plugin:', errorMessage)
@@ -51,36 +51,5 @@ export class SourcemapWebpackPlugin {
     } else {
       compiler.plugin('done', onDone)
     }
-  }
-
-  async runInject(): Promise<void> {
-    const cliOptions = []
-    cliOptions.push('sourcemap', 'inject', '--directory', this.directory)
-    await callPosthogCli(cliOptions, process.env, this.posthogOptions.verbose)
-  }
-
-  async runUpload(): Promise<void> {
-    const cliOptions = []
-    if (this.posthogOptions.host) {
-      cliOptions.push('--host', this.posthogOptions.host)
-    }
-    cliOptions.push('sourcemap', 'upload')
-    cliOptions.push('--directory', this.directory)
-    if (this.posthogOptions.sourcemaps.project) {
-      cliOptions.push('--project', this.posthogOptions.sourcemaps.project)
-    }
-    if (this.posthogOptions.sourcemaps.version) {
-      cliOptions.push('--version', this.posthogOptions.sourcemaps.version)
-    }
-    if (this.posthogOptions.sourcemaps.deleteAfterUpload && !this.isServer) {
-      cliOptions.push('--delete-after')
-    }
-    // Add env variables
-    const envVars = {
-      ...process.env,
-      POSTHOG_CLI_TOKEN: this.posthogOptions.personalApiKey,
-      POSTHOG_CLI_ENV_ID: this.posthogOptions.envId,
-    }
-    await callPosthogCli(cliOptions, envVars, this.posthogOptions.verbose)
   }
 }
