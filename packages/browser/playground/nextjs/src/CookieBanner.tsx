db136198143f84376ff9753a1efcfc68851d5313
Robbie Coomber <robbie.coomber@gmail.com>
2025-08-22 14:03:08 +0100

    Improve consent APIs

--- packages/browser/playground/nextjs/src/CookieBanner.tsx
+++ packages/browser/playground/nextjs/src/CookieBanner.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable posthog-js/no-direct-null-check */
 import { useEffect, useState, ReactElement } from 'react'
-import { cookieConsentGiven, posthog, updatePostHogConsent } from './posthog'
+import { ConsentState, cookieConsentGiven, posthog, updatePostHogConsent } from './posthog'
 
-export function useCookieConsent(): [boolean | null, (consentGiven: boolean) => void] {
-    const [consentGiven, setConsentGiven] = useState<boolean | null>(null)
+export function useCookieConsent(): [ConsentState | null, (consentGiven: ConsentState) => void] {
+    const [consentGiven, setConsentGiven] = useState<ConsentState | null>(null)
 
     useEffect(() => {
         setConsentGiven(cookieConsentGiven())
     }, [])
 
     useEffect(() => {
-        if (consentGiven === null || posthog.config.cookieless_mode === 'always') {
+        if (consentGiven == null || posthog.config.cookieless_mode === 'always') {
             return
         }
 
@@ -23,31 +23,66 @@ export function useCookieConsent(): [boolean | null, (consentGiven: boolean) => 
 export function CookieBanner(): ReactElement | null {
     const [consentGiven, setConsentGiven] = useCookieConsent()
 
-    if (consentGiven === null || posthog.config.cookieless_mode === 'always') {
+    // eslint-disable-next-line posthog-js/no-direct-undefined-check
+    if (consentGiven === undefined || posthog.config.cookieless_mode === 'always') {
         return null
     }
 
     return (
-        <div className="fixed right-2 bottom-2 border rounded p-2 bg-gray-100 text-sm">
-            {!consentGiven ? (
+        <div className="fixed right-2 bottom-2 border rounded p-2 bg-gray-100 text-sm space-y-2">
+            {consentGiven === null ? (
                 <>
                     <p>I am a cookie banner - you wouldn't like me when I'm hangry.</p>
-                    <button
-                        onClick={() => {
-                            setConsentGiven(true)
-                        }}
-                    >
-                        Approved!
-                    </button>
+                    <div className="space-x-2">
+                        <button
+                            onClick={() => {
+                                setConsentGiven(true)
+                            }}
+                        >
+                            Approved!
+                        </button>
+                        <button
+                            onClick={() => {
+                                setConsentGiven(false)
+                            }}
+                        >
+                            Denied!
+                        </button>
+                    </div>
                 </>
-            ) : (
+            ) : consentGiven ? (
                 <>
                     <button
                         onClick={() => {
                             setConsentGiven(false)
                         }}
                     >
-                        Give back my cookies!
+                        Give back my cookies! (revoke consent)
+                    </button>
+                    <button
+                        onClick={() => {
+                            posthog.reset()
+                        }}
+                    >
+                        Reset
+                    </button>
+                </>
+            ) : (
+                <>
+                    <button
+                        onClick={() => {
+                            setConsentGiven(true)
+                        }}
+                    >
+                        Ok you can have a cookie (accept cookies)
+                    </button>
+                    <button
+                        onClick={() => {
+                            posthog.reset()
+                            setConsentGiven(null)
+                        }}
+                    >
+                        Reset
                     </button>
                 </>
             )}
