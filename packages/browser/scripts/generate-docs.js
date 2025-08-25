35087768194033505f73f65627d03ac9d6ef88eb
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2025-08-25 16:23:42 -0400

    Clean up constants

--- packages/browser/scripts/generate-docs.js
+++ packages/browser/scripts/generate-docs.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const { generateApiSpecs } = require('../../../scripts/docs/parser');
-const { HOG_REF, SPEC_INFO, PROPERTIES_EXAMPLE, PROPERTY_EXAMPLE } = require('../../../scripts/docs/constants');
+const { HOG_REF, PROPERTIES_EXAMPLE, PROPERTY_EXAMPLE } = require('../../../scripts/docs/constants');
 
 const config = {
     packageDir: path.resolve(__dirname, '..'),  // packages/browser
     apiJsonPath: path.resolve(__dirname, '../docs/posthog-js.api.json'),
     outputPath: path.resolve(__dirname, '../docs/posthog-js-references.json'),
-    id: SPEC_INFO.id,
+    id: 'posthog-js',
     hogRef: HOG_REF,
-    specInfo: SPEC_INFO,
+    specInfo: {
+        id: 'posthog-js',
+        title: 'PostHog JavaScript Web SDK',
+        description: 'Posthog-js allows you to automatically capture usage and send events to PostHog.',
+        slugPrefix: 'posthog-js',
+        specUrl: 'https://github.com/PostHog/posthog-js'
+    },
     typeExamples: {
         Properties: PROPERTIES_EXAMPLE,
         Property: PROPERTY_EXAMPLE
