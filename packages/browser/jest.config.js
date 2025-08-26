82ace0a05870ceeaca08eccc8f29fe18a2c53b4b
pauldambra <paul@posthog.com>
2025-08-26 16:48:15 +0100

    fix

--- packages/browser/jest.config.js
+++ packages/browser/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+test.[jt]s?(x)'],
+    testMatch: [
+        '**/__tests__/**/*.test.[jt]s?(x)',
+        '**/?(*.)+test.[jt]s?(x)',
+        '**/functional_tests/**/*.test.[jt]s?(x)',
+    ],
     testPathIgnorePatterns: ['/node_modules/', '/cypress/', '/react/', '/test_data/', '/testcafe/'],
     moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
     setupFilesAfterEnv: ['./src/__tests__/setup.js'],
