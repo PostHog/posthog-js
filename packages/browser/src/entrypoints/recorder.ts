d2fd579759f5caf542c701fbe2ffcd70f386574a
pauldambra <paul@posthog.com>
2025-08-27 20:13:05 +0100

    fix

--- packages/browser/src/entrypoints/recorder.ts
+++ packages/browser/src/entrypoints/recorder.ts
@@ -5,9 +5,11 @@ import { assignableWindow } from '../utils/globals'
 import { LazyLoadedSessionRecording } from '../extensions/replay/external/lazy-loaded-session-recorder'
 
 assignableWindow.__PosthogExtensions__ = assignableWindow.__PosthogExtensions__ || {}
-assignableWindow.__PosthogExtensions__.rrwebPlugins = { getRecordConsolePlugin, getRecordNetworkPlugin }
-assignableWindow.__PosthogExtensions__.rrweb = { record: rrwebRecord, version: 'v2' }
-assignableWindow.__PosthogExtensions__.initSessionRecording = (ph) => new LazyLoadedSessionRecording(ph)
+// capture as a variable to try and help with bundling
+const extensions = assignableWindow.__PosthogExtensions__
+extensions.rrwebPlugins = { getRecordConsolePlugin, getRecordNetworkPlugin }
+extensions.rrweb = { record: rrwebRecord, version: 'v2' }
+extensions.initSessionRecording = (ph) => new LazyLoadedSessionRecording(ph)
 // we used to put all of these items directly on window, and now we put it on __PosthogExtensions__
 // but that means that old clients which lazily load this extension are looking in the wrong place
 // yuck,
