3763cd04d036dfea59d29a111975ea3c44204958
Carlos Marchal <carlos@posthog.com>
2025-09-03 15:22:09 +0200

    fix: enforce types for OpenAI params and warn

--- packages/ai/src/openai/utils.ts
+++ packages/ai/src/openai/utils.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid'
 import { MonitoringEventProperties, MonitoringEventPropertiesWithDefaults, MonitoringParams } from '../utils'
 
-const POSTHOG_PARAMS_MAP: Record<string, string> = {
+const POSTHOG_PARAMS_MAP: Record<keyof MonitoringParams, string> = {
   posthogDistinctId: 'distinctId',
   posthogTraceId: 'traceId',
   posthogProperties: 'properties',
@@ -21,12 +21,11 @@ export function extractPosthogParams<T>(body: T & MonitoringParams): {
   const posthogParams: Record<string, unknown> = {}
 
   for (const [key, value] of Object.entries(body)) {
-    if (POSTHOG_PARAMS_MAP[key]) {
-      posthogParams[POSTHOG_PARAMS_MAP[key]] = value
+    if (POSTHOG_PARAMS_MAP[key as keyof MonitoringParams]) {
+      posthogParams[POSTHOG_PARAMS_MAP[key as keyof MonitoringParams]] = value
+    } else if (key.startsWith('posthog')) {
+      console.warn(`Unknown Posthog parameter ${key}`)
     } else {
-      if (key.startsWith('posthog')) {
-        throw new Error(`Posthog parameter ${key} is being passed to the OpenAI client, and will cause it to fail`)
-      }
       openAIParams[key] = value
     }
   }
