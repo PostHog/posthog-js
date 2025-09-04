cb50cdc33657b632bf201f64098ebb29f0908f35
pauldambra <paul@posthog.com>
2025-09-04 13:20:15 +0100

    fix

--- packages/browser/src/posthog-surveys.ts
+++ packages/browser/src/posthog-surveys.ts
@@ -35,11 +35,11 @@ export class PostHogSurveys {
     }
 
     onRemoteConfig(response: RemoteConfig) {
+        // only load surveys if they are enabled and there are surveys to load
         if (this._instance.config.disable_surveys) {
             return
         }
 
-        // only load surveys if they are enabled and there are surveys to load
         const surveys = response['surveys']
         if (isNullish(surveys)) {
             return logger.warn('Flags not loaded yet. Not loading surveys.')
