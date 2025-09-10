dd12b2d81dc5c6d4aa785be18441e0e04927c84a
David Newell <david@posthog.com>
2025-09-10 09:40:28 +0100

    type event

--- packages/react/src/helpers/error-helpers.ts
+++ packages/react/src/helpers/error-helpers.ts
@@ -1,9 +1,10 @@
 import type { ErrorInfo } from 'react'
 import { PostHog } from '../context'
+import { CaptureResult } from 'posthog-js'
 
 export const setupReactErrorHandler = (
     client: PostHog,
-    callback?: (event: any, error: any, errorInfo: ErrorInfo) => void
+    callback?: (event: CaptureResult | undefined, error: any, errorInfo: ErrorInfo) => void
 ) => {
     return (error: any, errorInfo: ErrorInfo): void => {
         const event = client.captureException(error)
