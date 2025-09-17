93d63ae5b42c1475a506c38a04e10649149966de
Hugues Pouillot <hpouillot@gmail.com>
2025-09-17 14:55:32 +0200

    add dom exception

--- packages/browser/playwright/mocked/error-tracking/capture-exception.spec.ts
+++ packages/browser/playwright/mocked/error-tracking/capture-exception.spec.ts
@@ -58,6 +58,14 @@ test.describe('ErrorTracking captureException', () => {
         exceptionMatch(exception, 'Error', "Non-Error 'exception' captured with keys: message, name")
     })
 
+    test('captureException(DOMException)', async ({ posthog, events }) => {
+        const exception = await bootstrap(posthog, events, (ph) => {
+            const exceptionObject = new DOMException('exception message', 'exception name')
+            ph.captureException(exceptionObject)
+        })
+        exceptionMatch(exception, 'DOMException', 'exception name: exception message')
+    })
+
     test('captureException(ErrorEvent)', async ({ posthog, events }) => {
         const exception = await bootstrap(posthog, events, (ph) => {
             class CustomError extends Error {
