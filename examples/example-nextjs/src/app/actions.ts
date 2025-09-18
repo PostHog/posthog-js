13fbe0b73655c68ca492b69d053490f742e65ffd
Hugues Pouillot <hpouillot@gmail.com>
2025-09-18 15:58:36 +0200

    update nextjs example

---
+++ examples/example-nextjs/src/app/actions.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { PostHog } from 'posthog-node'
+
+const posthog = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_KEY!, {
+    host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+})
+
+export async function captureServerError() {
+    await posthog.captureExceptionImmediate(new Error('Server Exception'), 'distinct_id')
+}
--- examples/example-nextjs/src/app/page.tsx
+++ examples/example-nextjs/src/app/page.tsx
@@ -1,15 +1,25 @@
 'use client'
 import { usePostHog } from 'posthog-js/react'
+import { throwServerError } from './actions'
 
 export default function Home() {
     const posthog = usePostHog()
     return (
         <div>
             <main>
-                <div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '30px',
+                    }}
+                >
                     <button onClick={() => posthog.captureException(new Error('exception captured'))}>
-                        Send exception!
+                        Create client exception!
                     </button>
+                    <button onClick={() => throwServerError()}>Create server exception!</button>
                 </div>
             </main>
         </div>
