a065b67817d65889a4a14c60c90d9fd0cac4e1e6
Hugues Pouillot <hpouillot@gmail.com>
2025-09-18 18:07:09 +0200

    fix stackparser test

--- packages/browser/playwright/mocked/error-tracking/stackparser.spec.ts
+++ packages/browser/playwright/mocked/error-tracking/stackparser.spec.ts
@@ -4,15 +4,10 @@ import { test } from '../../fixtures'
 test.describe('ErrorTracking stackparser', () => {
     test.use({ url: '/playground/cypress/index.html' })
 
-    test('should parse frames in all browsers', async ({ posthog, events, browserName }) => {
+    test('should parse frames in all browsers', async ({ posthog, page, events }) => {
         await posthog.init()
-        await posthog.evaluate((ph) => {
-            return ph.captureException(new Error('test error'))
-        })
+        await page.click('[data-cy-exception-button]')
         const exception = await events.waitForEvent('$exception')
-        if (browserName === 'chromium') {
-            // webkit produces: "at unknown (http://localhost:2345/playground/cypress/index.html:1:0)"
-            expect(exception.properties.$exception_list[0].stacktrace.frames).toHaveLength(3)
-        }
+        expect(exception.properties.$exception_list[0].stacktrace.frames).toHaveLength(1)
     })
 })
