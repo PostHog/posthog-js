3d3fc37bfe351d9a464e652d8ec347192596769d
Lucas Faria <12522524+lucasheriques@users.noreply.github.com>
2025-09-20 05:23:22 -0300

    fix: mark renderSurvey as deprecated (#2344)

---
+++ .changeset/rude-memes-begin.md
@@ -0,0 +1,5 @@
+---
+'posthog-js': patch
+---
+
+chore: mark renderSurvey as deprecated
--- packages/browser/src/posthog-core.ts
+++ packages/browser/src/posthog-core.ts
@@ -1784,6 +1784,8 @@ export class PostHog {
      * posthog.renderSurvey(coolSurveyID, '#survey-container')
      * ```
      *
+     * @deprecated Use displaySurvey instead - it's more complete and also supports popover surveys.
+     *
      * @public
      *
      * @param {String} surveyId The ID of the survey to render.
