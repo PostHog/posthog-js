32a46ee9ac940cd3f47ea80bd533b62916ceeaa1
pauldambra <paul@posthog.com>
2025-09-23 10:08:09 +0100

    try jest 28

--- packages/browser/jest.config.js
+++ packages/browser/jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    testRunner: 'jest-circus/runner',
     testPathIgnorePatterns: ['/node_modules/', '/cypress/', '/react/', '/test_data/', '/testcafe/'],
     moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
     setupFilesAfterEnv: ['./src/__tests__/setup.js'],
@@ -8,5 +7,11 @@ module.exports = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '\\.css$': 'identity-obj-proxy',
+        '^preact$': '<rootDir>/node_modules/preact/dist/preact.js',
+        '^preact/hooks$': '<rootDir>/node_modules/preact/hooks/dist/hooks.js',
+        '^preact/test-utils$': '<rootDir>/node_modules/preact/test-utils/dist/testUtils.js',
+        '^preact/jsx-runtime$': '<rootDir>/node_modules/preact/jsx-runtime/dist/jsxRuntime.js',
+        '^@testing-library/preact$': '<rootDir>/node_modules/@testing-library/preact/dist/cjs/index.js',
     },
+    transformIgnorePatterns: ['node_modules/(?!(preact|@testing-library)/)'],
 }
