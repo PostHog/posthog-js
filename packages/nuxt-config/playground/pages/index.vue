040b3ef9e9a42dd93817be4f2eecb23b19d5255b
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-25 12:49:54 +0200

    feat: wip

--- packages/nuxt-config/playground/pages/index.vue
+++ packages/nuxt-config/playground/pages/index.vue
@@ -2,10 +2,12 @@
   <div>
     <h1>Home Page</h1>
     <p>This is the home page with an intentional error!</p>
+    <button @click="throwError5">Click to Throw Error</button>
   </div>
 </template>
 
 <script setup>
-// Throw an error in the frontend
-throw new Error('Frontend error thrown intentionally for testing!')
+const throwError5 = () => {
+  throw new Error('Frontend error thrown intentionally for testing!')
+}
 </script>
