f4f5e53de20d059ca67e529dda37f1c81b8b7d47
pauldambra <paul@posthog.com>
2025-10-08 16:16:32 +0100

    fix

--- packages/browser/jest.config.js
+++ packages/browser/jest.config.js
@@ -5,16 +5,9 @@ module.exports = {
     modulePathIgnorePatterns: ['src/__tests__/setup.js', 'src/__tests__/helpers/'],
     clearMocks: true,
     testEnvironment: 'jsdom',
+    resolver: '<rootDir>/jest-resolver.js',
     moduleNameMapper: {
         '\\.css$': 'identity-obj-proxy',
-        '^preact$': '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/dist/preact.js',
-        '^preact/hooks$': '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/hooks/dist/hooks.js',
-        '^preact/jsx-runtime$':
-            '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/jsx-runtime/dist/jsxRuntime.js',
-        '^preact/test-utils$':
-            '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/test-utils/dist/testUtils.js',
-        '^@testing-library/preact$':
-            '<rootDir>/../../node_modules/.pnpm/@testing-library+preact@3.2.4_preact@10.19.3/node_modules/@testing-library/preact/dist/cjs/index.js',
     },
     transform: {
         '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
