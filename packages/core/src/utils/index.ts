db4a2e992dce181c42a12dd063b98b44f330a756
ablaszkiewicz <kqmdjc8@gmail.com>
2025-10-09 10:10:35 +0200

    feat: move cli to a dedicated export

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -5,7 +5,6 @@ export * from './number-utils'
 export * from './string-utils'
 export * from './type-utils'
 export * from './promise-queue'
-export * from './call-posthog-cli'
 
 export const STRING_FORMAT = 'utf8'
 
