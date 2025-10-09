734110f5c4fd3995a156362e672210e232869350
ablaszkiewicz <kqmdjc8@gmail.com>
2025-10-09 16:15:15 +0200

    feat: wip

--- examples/example-nuxt-module/server/routes/error.ts
+++ examples/example-nuxt-module/server/routes/error.ts
@@ -1,3 +1,11 @@
 export default defineEventHandler((event) => {
+  setTimeout(() => {
+    throw new Error('Some unawaited nitro error')
+  }, 1000)
+
+  setTimeout(() => {
+    Promise.reject(new Error('Some uncaught nitro promise rejection'))
+  }, 1000)
+
   throw new Error('Test server error from Nitro')
 })
