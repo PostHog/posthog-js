1f294ecb3c816b283f04c0dacc01739d79a5805c
Hugues Pouillot <hpouillot@gmail.com>
2025-10-09 13:05:33 +0200

    feat(react-native): add autocapture handling (#2410)

--- examples/example-expo-53/app/posthog.tsx
+++ examples/example-expo-53/app/posthog.tsx
@@ -5,10 +5,19 @@ export const posthog = new PostHog(process.env.EXPO_PUBLIC_POSTHOG_PROJECT_KEY!,
     flushAt: 1,
     enableSessionReplay: true,
     captureAppLifecycleEvents: true,
+    errorTracking: {
+        autocapture: {
+            uncaughtExceptions: true,
+            unhandledRejections: true,
+            console: ['error', 'warn'],
+        },
+    },
+
     // if using WebView, you have to disable masking for text inputs and images
     // sessionReplayConfig: {
     //   maskAllTextInputs: false,
     //   maskAllImages: false,
     // },
 })
+
 posthog.debug(true)
