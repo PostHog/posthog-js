3fede8801af60fe953fd0c0120b9c3df6a05608c
Manoel Aranda Neto <marandaneto@gmail.com>
2025-11-10 19:27:15 +0100

    fix

--- packages/react-native/tooling/copy-chunkid.js
+++ packages/react-native/tooling/copy-chunkid.js
@@ -1,27 +1,27 @@
-// copied from https://github.com/getsentry/sentry-react-native/blob/e76d0d388228437e82f235546de00f4e748fcbda/packages/core/scripts/copy-chunkId.js
+// copied from https://github.com/getsentry/sentry-react-native/blob/e76d0d388228437e82f235546de00f4e748fcbda/packages/core/scripts/copy-debugid.js
 
 import { argv, exit } from 'process'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
 
-console.log('Copy `chunkId` from packager source map to Hermes source map...')
+console.log('Copy `debugId` from packager source map to Hermes source map...')
 
 const packagerSourceMapPath = argv[2]
 const hermesSourceMapPath = argv[3]
 
 if (!packagerSourceMapPath) {
-  console.log('Please provide packager source map path (A path to copy `chunkId` from).')
+  console.log('Please provide packager source map path (A path to copy `debugId` from).')
   exit(0)
 }
 if (!hermesSourceMapPath) {
-  console.log('Please provide Hermes source map path. ((A path to copy `chunkId` to))')
+  console.log('Please provide Hermes source map path. ((A path to copy `debugId` to))')
   exit(0)
 }
 if (!existsSync(packagerSourceMapPath)) {
-  console.log('Packager source map path (A path to copy `chunkId` from).')
+  console.log('Packager source map path (A path to copy `debugId` from).')
   exit(0)
 }
 if (!existsSync(hermesSourceMapPath)) {
-  console.log('Hermes source map not found. ((A path to copy `chunkId` to))')
+  console.log('Hermes source map not found. ((A path to copy `debugId` to))')
   exit(0)
 }
 
@@ -31,8 +31,8 @@ const to = readFileSync(argv[3], 'utf8')
 const fromParsed = JSON.parse(from)
 const toParsed = JSON.parse(to)
 
-if (!fromParsed.chunkId) {
-  console.log('Packager source map does not have `chunkId`.')
+if (!fromParsed.debugId) {
+  console.log('Packager source map does not have `debugId`.')
   exit(0)
 }
 
@@ -41,8 +41,8 @@ if (toParsed.chunkId) {
   exit(0)
 }
 
-if (fromParsed.chunkId) {
-  toParsed.chunkId = fromParsed.chunkId
+if (fromParsed.debugId) {
+  toParsed.chunkId = fromParsed.debugId
 }
 
 writeFileSync(argv[3], JSON.stringify(toParsed))
