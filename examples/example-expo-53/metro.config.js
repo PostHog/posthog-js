71504ed7a205e69911e96da3319ee73d84accda4
Manoel Aranda Neto <marandaneto@gmail.com>
2025-11-10 18:10:06 +0100

    fix

--- examples/example-expo-53/metro.config.js
+++ examples/example-expo-53/metro.config.js
@@ -1,2 +1,2 @@
-const { getPostHogExpoConfig } = require('posthog-react-native/tooling/metro')
+const { getPostHogExpoConfig } = require('posthog-react-native/metro')
 const config = getPostHogExpoConfig(__dirname)
