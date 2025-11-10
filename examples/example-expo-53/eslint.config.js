fa56f6ebbe43876d7c11ef6395b90baf7827abda
Manoel Aranda Neto <marandaneto@gmail.com>
2025-11-10 14:01:14 +0100

    rollback

--- examples/example-expo-53/eslint.config.js
+++ examples/example-expo-53/eslint.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('eslint/config')
 const expoConfig = require('eslint-config-expo/flat')
 
 module.exports = defineConfig([
-    {
-        ignores: ['dist/*', '**/metro.config.js', '**/eslint.config.js'],
-    },
     expoConfig,
+    {
+        ignores: ['dist/*'],
+    },
 ])
