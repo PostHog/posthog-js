68f759f7754e0bc75699d2d86496fd1e85040e6d
Manoel Aranda Neto <marandaneto@gmail.com>
2025-11-21 12:01:05 +0100

    chore: expo plugin rn install

--- packages/react-native/.eslintrc.cjs
+++ packages/react-native/.eslintrc.cjs
@@ -23,5 +23,5 @@ module.exports = {
       },
     },
   ],
-  ignorePatterns: ['src/version.ts', 'src/tooling/*', 'tooling/**', 'react-native/metro.js'],
+  ignorePatterns: ['src/version.ts', 'src/tooling/*', 'tooling/**'],
 }
---
+++ packages/react-native/src/tooling/expoconfig.ts
@@ -0,0 +1,68 @@
+const { withAppBuildGradle, withXcodeProject } = require('@expo/config-plugins')
+
+const withAndroidPlugin = (config: any) => {
+  return withAppBuildGradle(config, (config: any) => {
+    if (config.modResults.language !== 'groovy') {
+      console.warn('Cannot configure PostHog in the app gradle because the build.gradle is not groovy')
+    }
+
+    const buildGradle = config.modResults.contents
+    const lineToAdd = 'apply from: "../../node_modules/posthog-react-native/tooling/posthog.gradle"'
+
+    if (buildGradle.includes(lineToAdd)) {
+      return config
+    }
+
+    // Find the 'android {' block and insert the line directly above it
+    const pattern = /^android\s*\{/m
+
+    if (buildGradle.match(pattern)) {
+      config.modResults.contents = buildGradle.replace(pattern, `${lineToAdd}\n\nandroid {`)
+    } else {
+      console.warn('PostHog: Could not find "android {" block in build.gradle')
+    }
+
+    return config
+  })
+}
+
+const withIosPlugin = (config: any) => {
+  return withXcodeProject(config, (config: any) => {
+    const xcodeProject = config.modResults
+    const buildPhases = xcodeProject.hash.project.objects.PBXShellScriptBuildPhase
+
+    for (const key in buildPhases) {
+      const buildPhase = buildPhases[key]
+      const name = buildPhase.name ? buildPhase.name.replace(/"/g, '') : ''
+
+      if (name === 'Bundle React Native code and images') {
+        let script = buildPhase.shellScript
+
+        const oldSearch =
+          "require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'"
+
+        const newReplace =
+          "require('path').dirname(require.resolve('posthog-react-native')) + '/../tooling/posthog-xcode.sh'"
+
+        if (!script.includes('posthog-xcode.sh')) {
+          if (script.includes(oldSearch)) {
+            script = script.replace(oldSearch, newReplace)
+            buildPhase.shellScript = script
+          } else {
+            console.warn(
+              'PostHog: Could not find exact match for React Native script path. Verify your build phase script content.'
+            )
+          }
+        }
+        break
+      }
+    }
+    return config
+  })
+}
+
+module.exports = (config: any) => {
+  config = withAndroidPlugin(config)
+  //   return withIosPlugin(config);
+  return config
+}
