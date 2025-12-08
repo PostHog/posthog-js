03cf322c5670a2f8914e6a7910887c6109b3ce65
Robbie Coomber <robbie@posthog.com>
2025-12-08 18:01:13 +0000

    Remove preact entries from moduleNameMapper

--- packages/browser/jest.config.js
+++ packages/browser/jest.config.js
@@ -9,14 +9,6 @@ module.exports = {
     prettierPath: null,
     moduleNameMapper: {
         '\\.css$': 'identity-obj-proxy',
-        '^preact$': '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/dist/preact.js',
-        '^preact/hooks$': '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/hooks/dist/hooks.js',
-        '^preact/jsx-runtime$':
-            '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/jsx-runtime/dist/jsxRuntime.js',
-        '^preact/test-utils$':
-            '<rootDir>/../../node_modules/.pnpm/preact@10.19.3/node_modules/preact/test-utils/dist/testUtils.js',
-        '^@testing-library/preact$':
-            '<rootDir>/../../node_modules/.pnpm/@testing-library+preact@3.2.4_preact@10.19.3/node_modules/@testing-library/preact/dist/cjs/index.js',
     },
     transform: {
         '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
