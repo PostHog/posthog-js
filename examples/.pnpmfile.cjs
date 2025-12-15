b097ff12b2546cc2824493d0a117134dee876011
Hugues Pouillot <hpouillot@gmail.com>
2025-12-15 13:52:41 +0100

    move nodeLinker option to project settings

--- examples/.pnpmfile.cjs
+++ examples/.pnpmfile.cjs
@@ -29,7 +29,6 @@ module.exports = {
                 packages: ['.'],
                 preferFrozenLockfile: false,
                 verifyDepsBeforeRun: true,
-                nodeLinker: 'isolated',
                 minimumReleaseAge: 4320,
                 minimumReleaseAgeExclude: ['node-forge@1.3.2', '@posthog/cli'],
                 onlyBuiltDependencies: ['@posthog/cli'],
