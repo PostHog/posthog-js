a6e2d47b40d8fbb447a1081b505f93a872c3435d
Frank Hamand <frank@posthog.com>
2025-12-17 16:10:21 +0000

    Add logs extension to posthog-js

---
+++ packages/browser/src/posthog-logs.ts
@@ -0,0 +1,51 @@
+import { PostHog } from './posthog-core'
+import { RemoteConfig } from './types'
+import { isNullish } from '@posthog/core'
+import { assignableWindow } from './utils/globals'
+import { createLogger } from './utils/logger'
+
+export class PostHogLogs {
+    private _hasLogs?: boolean
+
+    constructor(private readonly _instance: PostHog) {}
+
+    onRemoteConfig(response: RemoteConfig) {
+        // only load logs if they are enabled
+        const logCapture = response.logs?.captureConsoleLogs
+        if (isNullish(logCapture) || !logCapture) {
+            return
+        }
+        this._hasLogs = true
+        this.loadIfEnabled()
+    }
+
+    reset(): void {}
+
+    loadIfEnabled() {
+        if (!this._hasLogs) {
+            return
+        }
+
+        const logger = createLogger('[logs]')
+        const phExtensions = assignableWindow?.__PosthogExtensions__
+        if (!phExtensions) {
+            logger.error('PostHog Extensions not found.')
+            return
+        }
+
+        const loadExternalDependency = phExtensions.loadExternalDependency
+        if (!loadExternalDependency) {
+            logger.error('PostHog loadExternalDependency extension not found.')
+            return
+        }
+
+        loadExternalDependency(this._instance, 'logs', (err) => {
+            if (err || !phExtensions.initializeLogs) {
+                logger.error('Could not load logs script', err)
+            } else {
+                // Need to get the function reference again inside the callback
+                phExtensions.initializeLogs(this._instance)
+            }
+        })
+    }
+}
