d5b645cab1777120538b567ad16caab418430268
Hugues Pouillot <hpouillot@gmail.com>
2025-12-22 20:01:49 +0100

    chore: add vite playground (#2808)
    * add vite playground
    
    * Update playground/vite/package.json
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

---
+++ playground/vite/vite.config.ts
@@ -0,0 +1,16 @@
+/* eslint-env node */
+import posthog from '@posthog/rollup-plugin'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [
+        posthog({
+            personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY!,
+            envId: process.env.POSTHOG_PROJECT_ID!,
+            host: process.env.POSTHOG_API_HOST,
+            sourcemaps: {
+                enabled: true,
+            },
+        }),
+    ],
+})
