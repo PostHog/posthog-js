dae6346f365a25452888da6d4ac64015dca5ccf6
Adam Bowker <adam.b@posthog.com>
2025-12-22 10:29:26 -0800

    feat(product tours): session store for multi-page tours

---
+++ .changeset/strict-words-retire.md
@@ -0,0 +1,5 @@
+---
+'posthog-js': patch
+---
+
+support multi-page product tours
--- packages/browser/src/extensions/product-tours/constants.ts
+++ packages/browser/src/extensions/product-tours/constants.ts
@@ -1,2 +1,3 @@
 export const TOUR_COMPLETED_KEY_PREFIX = 'ph_product_tour_completed_'
 export const TOUR_DISMISSED_KEY_PREFIX = 'ph_product_tour_dismissed_'
+export const ACTIVE_TOUR_SESSION_KEY = 'ph_active_product_tour'
