c17b44ac1129d6f5581174a1d95b8be113f81e93
Adam Bowker <adam.b@posthog.com>
2026-01-13 09:40:20 -0800

    feat(product tours): add button action options (#2863)
    ## Problem
    
    want the ability to customize what the buttons in a product tour do. main app adds this for announcements here: https://github.com/PostHog/posthog/pull/44579
    
    <!-- Who are we building for, what are their needs, why is this important? -->
    
    ## Changes
    
    updates sdk to support custom buttons in tour steps, including some actions (next/prev/trigger_tour) that are not in the main app yet (soon)
    
    also fixes a bug with click-outside-to-dismiss, and adds an explicit setting for this at the tour level. type added in this PR: https://github.com/PostHog/posthog/pull/44573 but not user-configurable yet... defaults to false for announcements so clicking outside won't get rid of them
    
    <!-- What is changed and what information would be useful to a reviewer? -->
    
    ## Release info Sub-libraries affected
    
    ### Libraries affected
    
    <!-- Please mark which libraries will require a version bump. -->
    
    - [ ] All of them
    - [x] posthog-js (web)
    - [ ] posthog-js-lite (web lite)
    - [ ] posthog-node
    - [ ] posthog-react-native
    - [ ] @posthog/react
    - [ ] @posthog/ai
    - [ ] @posthog/nextjs-config
    - [ ] @posthog/nuxt
    - [ ] @posthog/rollup-plugin
    - [ ] @posthog/webpack-plugin
    
    ## Checklist
    
    - [ ] Tests for new code
    - [x] Accounted for the impact of any changes across different platforms
    - [x] Accounted for backwards compatibility of any changes (no breaking changes!)
    - [x] Took care not to unnecessarily increase the bundle size
    
    ### If releasing new changes
    
    - [x] Ran `pnpm changeset` to generate a changeset file
    - [x] Added the "release" label to the PR to indicate we're publishing new versions for the affected packages
    
    <!-- For more details check RELEASING.md -->

--- packages/browser/src/extensions/product-tours/components/ProductTourTooltipInner.tsx
+++ packages/browser/src/extensions/product-tours/components/ProductTourTooltipInner.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact'
-import { ProductTourStep, ProductTourAppearance } from '../../../posthog-product-tours-types'
+import { ProductTourStep, ProductTourAppearance, ProductTourStepButton } from '../../../posthog-product-tours-types'
 import { getStepHtml } from '../product-tours-utils'
 import { IconPosthogLogo, cancelSVG } from '../../surveys/icons'
 
@@ -11,6 +11,7 @@ export interface ProductTourTooltipInnerProps {
     onNext?: () => void
     onPrevious?: () => void
     onDismiss?: () => void
+    onButtonClick?: (button: ProductTourStepButton) => void
 }
 
 export function ProductTourTooltipInner({
@@ -21,14 +22,22 @@ export function ProductTourTooltipInner({
     onNext,
     onPrevious,
     onDismiss,
+    onButtonClick,
 }: ProductTourTooltipInnerProps): h.JSX.Element {
     const whiteLabel = appearance?.whiteLabel ?? false
     const isLastStep = stepIndex >= totalSteps - 1
     const isFirstStep = stepIndex === 0
-    const showNextButton = step.progressionTrigger === 'button' || step.type === 'modal'
+    const showDefaultButtons = !step.buttons && (step.progressionTrigger === 'button' || step.type === 'modal')
+    const hasCustomButtons = !!step.buttons
 
-    const isInteractive = !!(onNext || onPrevious || onDismiss)
+    const isInteractive = !!(onNext || onPrevious || onDismiss || onButtonClick)
     const cursorStyle = isInteractive ? undefined : { cursor: 'default' }
+
+    const handleButtonClick = (button: ProductTourStepButton) => {
+        if (onButtonClick) {
+            onButtonClick(button)
+        }
+    }
 
     return (
         <>
@@ -39,24 +48,53 @@ export function ProductTourTooltipInner({
             <div class="ph-tour-content" dangerouslySetInnerHTML={{ __html: getStepHtml(step) }} />
 
             <div class="ph-tour-footer">
-                <span class="ph-tour-progress">
-                    {stepIndex + 1} of {totalSteps}
-                </span>
+                {totalSteps > 1 && (
+                    <span class="ph-tour-progress">
+                        {stepIndex + 1} of {totalSteps}
+                    </span>
+                )}
 
                 <div class="ph-tour-buttons">
-                    {!isFirstStep && (
-                        <button
-                            class="ph-tour-button ph-tour-button--secondary"
-                            onClick={onPrevious}
-                            style={cursorStyle}
-                        >
-                            Back
-                        </button>
+                    {/* Default buttons for tours without custom buttons */}
+                    {showDefaultButtons && (
+                        <>
+                            {!isFirstStep && (
+                                <button
+                                    class="ph-tour-button ph-tour-button--secondary"
+                                    onClick={onPrevious}
+                                    style={cursorStyle}
+                                >
+                                    Back
+                                </button>
+                            )}
+                            <button class="ph-tour-button ph-tour-button--primary" onClick={onNext} style={cursorStyle}>
+                                {isLastStep ? 'Done' : 'Next'}
+                            </button>
+                        </>
                     )}
-                    {showNextButton && (
-                        <button class="ph-tour-button ph-tour-button--primary" onClick={onNext} style={cursorStyle}>
-                            {isLastStep ? 'Done' : 'Next'}
-                        </button>
+
+                    {/* Custom buttons */}
+                    {hasCustomButtons && (
+                        <>
+                            {step.buttons?.secondary && (
+                                <button
+                                    class="ph-tour-button ph-tour-button--secondary"
+                                    onClick={() => handleButtonClick(step.buttons!.secondary!)}
+                                    style={cursorStyle}
+                                >
+                                    {step.buttons.secondary.text}
+                                </button>
+                            )}
+                            {step.buttons?.primary && (
+                                <button
+                                    class="ph-tour-button ph-tour-button--primary"
+                                    onClick={() => handleButtonClick(step.buttons!.primary!)}
+                                    style={cursorStyle}
+                                >
+                                    {step.buttons.primary.text}
+                                </button>
+                            )}
+                        </>
                     )}
                 </div>
             </div>
