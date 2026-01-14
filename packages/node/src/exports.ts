81b1370ede62042decd790103834ca6458c6bcbe
Matheus Veras Batista <matheus@posthog.com>
2026-01-14 12:11:19 -0300

    keep backwards compat with flag error types

--- packages/node/src/exports.ts
+++ packages/node/src/exports.ts
@@ -1,3 +1,4 @@
 export * from './extensions/sentry-integration'
 export * from './extensions/express'
 export * from './types'
+export { FeatureFlagError, FeatureFlagErrorType } from '@posthog/core'
