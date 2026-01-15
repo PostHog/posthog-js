e332e6eda05b2275a8d5deea2315c89901f076ef
luke-belton <luke.b@posthog.com>
2026-01-15 15:32:57 +0000

    remove `startUserFeedbackRecording` from client

--- packages/browser/src/extensions/feedback-recording/components/StartFeedbackRecordingUI.tsx
+++ packages/browser/src/extensions/feedback-recording/components/StartFeedbackRecordingUI.tsx
@@ -8,8 +8,8 @@ export function StartFeedbackRecordingUI({ handleStart, handleCancel }: StartFee
         <div className="feedback-recording-modal">
             <h2 className="feedback-recording-title">Record your screen</h2>
             <p className="feedback-recording-description">
-                Record your screen to explain more about the issue you are facing. PostHog support will be able to watch
-                this back and listen to audio - so feel free to talk us through the problem as you go!
+                Explain your problem with a screen recording and audio capture. Our support team will watch this and get
+                back to you soon!
             </p>
             <div className="feedback-recording-buttons">
                 <button className="feedback-recording-button feedback-recording-button-start" onClick={handleStart}>
