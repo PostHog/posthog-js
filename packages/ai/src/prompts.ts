546965247572f0c9d83f8820d0fd5b0a4c52235b
Radu Raicea <radu@raicea.com>
2026-01-28 14:14:54 -0500

    fix(llma): ci

--- packages/ai/src/prompts.ts
+++ packages/ai/src/prompts.ts
@@ -1,3 +1,5 @@
+/// <reference lib="dom" />
+
 import type { PostHog } from 'posthog-node'
 import type { CachedPrompt, GetPromptOptions, PromptApiResponse, PromptVariables, PromptsDirectOptions } from './types'
 
