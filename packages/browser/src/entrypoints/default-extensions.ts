3f7a54af0800683560eed7fbf2b6c86f8ce57575
Dustin Byrne <dustin@posthog.com>
2026-02-12 15:01:15 -0500

    feat: Export the bundle
    Why not

--- packages/browser/src/entrypoints/default-extensions.ts
+++ packages/browser/src/entrypoints/default-extensions.ts
@@ -1,24 +1,6 @@
 import { PostHog } from '../posthog-core'
-import { Autocapture } from '../autocapture'
-import { DeadClicksAutocapture } from '../extensions/dead-clicks-autocapture'
-import { ExceptionObserver } from '../extensions/exception-autocapture'
-import { HistoryAutocapture } from '../extensions/history-autocapture'
-import { TracingHeaders } from '../extensions/tracing-headers'
-import { WebVitalsAutocapture } from '../extensions/web-vitals'
-import { SessionRecording } from '../extensions/replay/session-recording'
-import { Heatmaps } from '../heatmaps'
-import { PostHogProductTours } from '../posthog-product-tours'
-import { SiteApps } from '../site-apps'
+import { AllExtensions } from '../extensions/extension-bundles'
 
 PostHog.__defaultExtensionClasses = {
-    historyAutocapture: HistoryAutocapture,
-    tracingHeaders: TracingHeaders,
-    siteApps: SiteApps,
-    sessionRecording: SessionRecording,
-    autocapture: Autocapture,
-    productTours: PostHogProductTours,
-    heatmaps: Heatmaps,
-    webVitalsAutocapture: WebVitalsAutocapture,
-    exceptionObserver: ExceptionObserver,
-    deadClicksAutocapture: DeadClicksAutocapture,
+    ...AllExtensions,
 }
--- packages/browser/src/entrypoints/module.slim.es.ts
+++ packages/browser/src/entrypoints/module.slim.es.ts
@@ -1,5 +1,6 @@
-// Experimental
-// This bundle is not currently exported
+console.warn(
+    '[PostHog Experimental] The slim module is experimental and may break or change on any release. Please use with caution and report any issues you encounter.'
+)
 import './external-scripts-loader'
 import posthog from './module.slim.no-external.es'
 export * from './module.slim.no-external.es'
