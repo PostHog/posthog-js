6088b0a02419516421c86431d50fa9ce04cb3667
Dustin Byrne <dustin@posthog.com>
2026-02-12 17:02:25 -0500

    chore: ignore an eslint rule

--- packages/browser/src/entrypoints/module.slim.es.ts
+++ packages/browser/src/entrypoints/module.slim.es.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-console */
 console.warn(
     '[PostHog Experimental] The slim module is experimental and may break or change on any release. Please use with caution and report any issues you encounter.'
 )
