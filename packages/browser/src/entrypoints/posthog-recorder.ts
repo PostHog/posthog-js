6bfa003116aa8ef65fe605c29ffee711f4d39b74
Paul D'Ambra <paul@posthog.com>
2026-02-23 09:08:06 +0000

    fix: putting rrweb directly on window for 1.16x.x compatability (#3129)
    we used to put rrweb and friends directly on window and still do for backwards compatabilty
    
    however, the old clients that look for it on window would never load posthog-recorder because they predate it
    
    so we need to do this on the "old" recorder entrypoint but not the new posthog-recorder entrypoint

--- packages/browser/src/entrypoints/posthog-recorder.ts
+++ packages/browser/src/entrypoints/posthog-recorder.ts
@@ -14,13 +14,4 @@ assignableWindow.__PosthogExtensions__.rrweb = {
 }
 assignableWindow.__PosthogExtensions__.initSessionRecording = (ph) => new LazyLoadedSessionRecording(ph)
 
-// we used to put all of these items directly on window, and now we put it on __PosthogExtensions__
-// but that means that old clients which lazily load this extension are looking in the wrong place
-// yuck,
-// so we also put them directly on the window
-// when 1.161.1 is the oldest version seen in production we can remove this
-assignableWindow.rrweb = { record: rrwebRecord, version: 'v2' }
-assignableWindow.rrwebConsoleRecord = { getRecordConsolePlugin }
-assignableWindow.getRecordNetworkPlugin = getRecordNetworkPlugin
-
 export default rrwebRecord
