51e1700f882a325bb36dc40105d03a33cd59c536
Kyle Swank <kyle@posthog.com>
2026-03-20 10:15:00 -0400

    Change to logger.warn instead of logger.error

--- packages/browser/src/extensions/replay/session-recording.ts
+++ packages/browser/src/extensions/replay/session-recording.ts
@@ -269,7 +269,7 @@ export class SessionRecording implements Extension {
 
     private _onScriptLoaded(startReason?: SessionStartReason) {
         if (!assignableWindow.__PosthogExtensions__?.initSessionRecording) {
-            logger.error('Called on script loaded before session recording is available. This can be caused by adblockers.')
+            logger.warn('Called on script loaded before session recording is available. This can be caused by adblockers.')
             this._instance.register_for_session({
                 $sdk_debug_recording_script_not_loaded: true,
             })
