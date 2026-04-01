d862184f175f3e317a267190cdfaa12fa72c5f15
Manoel Aranda Neto <marandaneto@gmail.com>
2026-04-01 09:04:06 +0200

    refactor: extract CLI/sourcemap utilities from @posthog/core into @posthog/plugin-utils

--- packages/plugin-utils/src/index.ts
+++ packages/plugin-utils/src/index.ts
@@ -1,4 +1,4 @@
 export * from './spawn-local'
-export { resolveBinaryPath } from './utils'
+export { resolveBinaryPath, buildLocalBinaryPaths } from './utils'
 export * from './config'
 export * from './cli'
