65d388cc49dcabc428d7c3afa834df9ab7f70085
Manoel Aranda Neto <marandaneto@gmail.com>
2026-08-12 16:30:05 +0200

    fix(react-native): use matching Hermes compiler in SwiftPM fixture

--- examples/example-rn-spm/ios/PosthogReactNativeSpmExample/AppDelegate.swift
+++ examples/example-rn-spm/ios/PosthogReactNativeSpmExample/AppDelegate.swift
@@ -39,10 +39,10 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   }
 
   override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+    if let bundledJavaScript = Bundle.main.url(forResource: "main", withExtension: "jsbundle") {
+      return bundledJavaScript
+    }
+
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
   }
 }
