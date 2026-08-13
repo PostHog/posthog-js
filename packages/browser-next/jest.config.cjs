c841c83d56c4f0168d4c9f77564e8ca561c9cfec
Dustin Byrne <dustin@posthog.com>
2026-08-12 23:35:22 -0400

    test(browser): share extension client conformance suite

--- packages/browser-next/jest.config.cjs
+++ packages/browser-next/jest.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
     testEnvironment: 'node',
     maxWorkers: 1,
     testMatch: ['<rootDir>/tests/**/*.spec.ts'],
+    moduleNameMapper: {
+        '^@posthog/browser-common/tests/client-conformance$':
+            '<rootDir>/../browser-common/tests/helpers/client-conformance.ts',
+    },
     transform: {
         '^.+\\.ts$': [
             'ts-jest',
