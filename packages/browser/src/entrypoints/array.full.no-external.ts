bbfe329c8c87d89a63031e63bbcec024963844aa
vaibhavmashal <vaibhavmashal098@gmail.com>
2026-08-24 00:44:29 +0530

    Fix toolbar loading in no-external builds

--- packages/browser/src/entrypoints/array.full.no-external.ts
+++ packages/browser/src/entrypoints/array.full.no-external.ts
@@ -1,4 +1,5 @@
 // Same as loader-globals.ts except includes all additional extension loaders
 import './all-external-dependencies'
 import './lazy-recorder'
+import './external-scripts-loader'
 import './array.no-external'
--- packages/browser/src/entrypoints/module.full.no-external.es.ts
+++ packages/browser/src/entrypoints/module.full.no-external.es.ts
@@ -1,5 +1,6 @@
 import './all-external-dependencies'
 import './lazy-recorder'
+import './external-scripts-loader'
 import posthog from './module.no-external.es'
 export * from './module.no-external.es'
 export default posthog
