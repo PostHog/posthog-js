41ed3af41c1a98776d1686caf4e58875f95b0847
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2026-09-03 22:54:23 +0700

    fix(browser): require a distinct ID for identify (#4760)

--- packages/browser/src/posthog-core.ts
+++ packages/browser/src/posthog-core.ts
@@ -3027,12 +3027,12 @@ export class PostHog implements PostHogInterface {
      *
      * @public
      *
-     * @param {String} [new_distinct_id] A string that uniquely identifies a user. If not provided, the distinct_id currently in the persistent store (cookie or localStorage) will be used.
+     * @param {String} [new_distinct_id] A non-empty string that uniquely identifies a user.
      * @param {Object} [userPropertiesToSet] Optional: An associative array of properties to store about the user. Note: For feature flag evaluations, if the same key is present in the userPropertiesToSetOnce,
      *  it will be overwritten by the value in userPropertiesToSet.
      * @param {Object} [userPropertiesToSetOnce] Optional: An associative array of properties to store about the user. If property is previously set, this does not override that value.
      */
-    identify(new_distinct_id?: string, userPropertiesToSet?: Properties, userPropertiesToSetOnce?: Properties): void {
+    identify(new_distinct_id: string, userPropertiesToSet?: Properties, userPropertiesToSetOnce?: Properties): void {
         if (!this.__loaded || !this.persistence) {
             return logger.uninitializedWarning('posthog.identify')
         }
